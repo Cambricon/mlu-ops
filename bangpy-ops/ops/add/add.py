a5d57f36b25d0b3279c6e538448f71a6073b3d14
zhourihua <uniquesquirrelzy@gmail.com>
2021-12-22 19:41:50 +0800

    feat(bangpy-ops): update add operator's computation.

--- bangpy-ops/ops/add/add.py
+++ bangpy-ops/ops/add/add.py
@@ -52,13 +52,20 @@ class Add(object):
 
     def compute_body(self):
         # calculate split strategy
-        assert self.length % self.task_num % ALIGN_LENGTH == 0
-        core_wl = 3 * self.length // self.task_num
-        loop_num = np.ceil(core_wl * self.dtype_sz / self.nram_size)
-        core_wl //= 3
-        while core_wl % loop_num != 0 or core_wl // loop_num % ALIGN_LENGTH != 0:
+        # ensure the data size can be divisible by task_num and 128 bytes aligned
+        assert (self.dtype_sz * self.length) % self.task_num % ALIGN_LENGTH == 0
+        # gets the data length to be calculated for each task
+        data_calculated_each_task = self.length // self.task_num
+        loop_num = np.ceil(
+            3 * data_calculated_each_task * self.dtype_sz / self.nram_size
+        )
+        # ensure the data size is 128 bytes aligned for each calculation
+        while (
+            data_calculated_each_task % loop_num != 0
+            or data_calculated_each_task // loop_num % ALIGN_LENGTH != 0
+        ):
             loop_num += 1
-        loop_wl = int(core_wl // loop_num)
+        data_calculated_each_time = int(data_calculated_each_task // loop_num)
         # declare I/O buffer
         buffer_in0 = self.bp.Buffer(
             shape=self.shape, name="INPUT0", dtype=self.dtype, scope="global"
@@ -72,18 +79,27 @@ class Add(object):
         task_id = self.bp.taskId
         # declare on-chip buffer
         buffer_in0_n = self.bp.Buffer(
-            shape=(loop_wl,), name="INPUT0_N", dtype=self.dtype, scope="nram"
+            shape=(data_calculated_each_time,),
+            name="INPUT0_N",
+            dtype=self.dtype,
+            scope="nram",
         )
         buffer_in1_n = self.bp.Buffer(
-            shape=(loop_wl,), name="INPUT1_N", dtype=self.dtype, scope="nram"
+            shape=(data_calculated_each_time,),
+            name="INPUT1_N",
+            dtype=self.dtype,
+            scope="nram",
         )
         buffer_out_n = self.bp.Buffer(
-            shape=(loop_wl,), name="OUTPUT_N", dtype=self.dtype, scope="nram"
+            shape=(data_calculated_each_time,),
+            name="OUTPUT_N",
+            dtype=self.dtype,
+            scope="nram",
         )
         # split and compute
         with self.bp.for_range(0, loop_num) as i:
-            start = task_id * core_wl + i * loop_wl
-            stop = start + loop_wl
+            start = task_id * data_calculated_each_task + i * data_calculated_each_time
+            stop = start + data_calculated_each_time
             self.bp.memcpy(buffer_in0_n, buffer_in0[start:stop])
             self.bp.memcpy(buffer_in1_n, buffer_in1[start:stop])
             self.bp.add(buffer_out_n, buffer_in0_n, buffer_in1_n)
@@ -99,7 +115,7 @@ class Add(object):
 
 @tcp.register_mlu_op(DTYPES, TRAGET_LIST, KERNEL_NAME)
 def build_add(dtype=None, target=None):
-    # tasktype fixed in Union1
+    # tasktype fixed in UNION1
     task_num = 4
     f = Add(SHAPE, dtype, target, task_num).compute_body()
     return f
