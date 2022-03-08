8aadb8569974a882ce750951301df7f119027336
gala <121872426@qq.com>
2022-03-08 23:13:03 +0800

    优化数据分配

--- bangpy-ops/ops/logaddexp/logaddexp.py
+++ bangpy-ops/ops/logaddexp/logaddexp.py
@@ -41,128 +41,72 @@ class LogAddExp(object):
     def __init__(self, dtype, target, task_num):#self 即this指针  dtype 传入的数据类型 target目标芯片的型号
         self.dtype = dtype
         self.target = target
-        self.task_num = task_num
-       
+        self.task_num = task_num     
         self.bp = tcp.TCP(target)
         self.length = self.bp.SizeVar("length")#得到数据的长度  此处应该是数组的长度
+        
         self.nram_size = TARGET(target).nram_size#每个核的Nram大小 每个core自己的存储空间
         self.dtype_sz = dtype.bytes#类型占用空间的大小(字节)
-        self.single_buffer_size =1024#每个buffer 占用的空间大小
+        self.single_buffer_size =(512-30-30)/2*1024#每个buffer 占用的空间大小 此时是两个输入 所以除2 又因系统堆栈要30所以减30  多减30预留
         self.bp.launch_task(self.task_num, 1, 1)#将任务维度值设置为在此内核中启动。  三个参数其实就是 taskdimx,y,z   
     def compute_body(self):
         # calculate split strategy
         # gets the data length to be calculated for each task
-        #因为要进行128字节对齐 这里向下取整 不足128字节的将会被舍弃掉不进行运算
-        data_calculated_each_task = self.length // self.task_num #数据长度除以投入的核心数 向下取整 得出每个核心计算的长度
-        # insufficient_one_core_length = self.length % self.task_num #取余 确认是否有没参加运算的数据
-        # gets the number of cycles required for each task
-        loop_num = data_calculated_each_task * self.dtype_sz // self.single_buffer_size #每次计算的长度乘以数据类型大小得到数据所占字节数（每个核分到的） 除以核内单个缓冲区的大小 来计算出每个核需要循环计算的次数（向下取整）
-        tail_size = (data_calculated_each_task* self.dtype_sz  % self.single_buffer_size) / self.dtype_sz#每个核 分到的数据长度 取余运算  确认是否有没有原酸的数据
-        # gets the data length for each calculation
-        data_calculated_each_time = self.single_buffer_size // self.dtype_sz   #核内单个缓冲区大小除以数据类型大小 得到一个核一次可以处理多长的数据
-       
-        # declare I/O buffer       
-        buffer_in0 = self.bp.Buffer(
+        one_core_count = self.length // self.task_num#每个核均摊计算量（按索引分）
+        remain = self.length % self.task_num#分任务时的余数
+        current_core_start = 0 #当前核心数据开始索引
+        current_core_end = 0 #当前核心数据结束索引
+        process_count = self.single_buffer_size // self.dtype_sz #核心一次最多计算的长度
+        with self.bp.if_scope(self.bp.taskId < remain): #如果存在余数 将其均摊给各核   taskId从0起
+            current_core_start = (one_core_count + 1) * self.bp.taskId 
+            current_core_end = (one_core_count + 1) * (self.bp.taskId + 1) - 1 #此处应该不需要减1 待验证  python切片会自动将上标减1
+        with self.bp.else_scope():
+            current_core_start = (one_core_count + 1) * remain + one_core_count * (self.bp.taskId - remain)
+            current_core_end = (one_core_count + 1) * remain + one_core_count * (self.bp.taskId - remain) + one_core_count - 1
+        
+        total_count_in_core = current_core_end - current_core_start + 1
+
+        gram_buffer_in0 = self.bp.Buffer(
             shape=(self.length,), name="INPUT0", dtype=self.dtype, scope="global"
         )
-        buffer_in1 = self.bp.Buffer(
+        gram_buffer_in1 = self.bp.Buffer(
             shape=(self.length,), name="INPUT1", dtype=self.dtype, scope="global"
         )
-        buffer_out = self.bp.Buffer(
+        gram_buffer_out = self.bp.Buffer(
             shape=(self.length,), name="OUTPUT", dtype=self.dtype, scope="global"
         )
-        # self.bp.print("self.length",self.length)
-        task_id = self.bp.taskId
-        # declare on-chip buffer
-        buffer_in0_n = self.bp.Buffer(
-            shape=(data_calculated_each_time,),
+        nram_buffer_in0 = self.bp.Buffer(
+            shape=(process_count,),
             name="INPUT0_N",
             dtype=self.dtype,
             scope="nram",
         ) 
-        buffer_in1_n = self.bp.Buffer(
-            shape=(data_calculated_each_time,),
+        nram_buffer_in1 = self.bp.Buffer(
+            shape=(process_count,),
             name="INPUT1_N",
             dtype=self.dtype,
             scope="nram",
-        )       
-        natural_power_x=self.bp.Buffer(#e的x次方
-            shape=(data_calculated_each_time,),
-            name="NPX",
-            dtype=self.dtype,
-            scope="nram",
         )
-        natural_power_y=self.bp.Buffer(#e的x次方
-            shape=(data_calculated_each_time,),
-            
-            name="NPY",
-            dtype=self.dtype,
-            scope="nram",
-        )
-        antilogarithm=self.bp.Buffer(#真数 
-            shape=(data_calculated_each_time,),
-            name="ANTI",
-            dtype=self.dtype,
-            scope="nram",
-        )
-       
+
+        calc_loop_count = (total_count_in_core + process_count - 1) // process_count
+        with self.bp.for_range(calc_loop_count) as i:            
+            once_loop_start = current_core_start + process_count * i #当前核心数据开始的位置 + 第i次循环所应偏移的长度
+            once_loop_end = once_loop_start + process_count - 1
+            with self.bp.if_scope(once_loop_end > current_core_start + total_count_in_core + 1):
+                once_loop_end = once_loop_start + total_count_in_core % process_count - 1
+            #print(once_loop_start, once_loop_end)
+            calc_size = once_loop_end - once_loop_start + 1
+            self.bp.memcpy(nram_buffer_in0[:calc_size], gram_buffer_in0[once_loop_start:once_loop_end + 1]) 
+            self.bp.memcpy(nram_buffer_in1[:calc_size], gram_buffer_in1[once_loop_start:once_loop_end + 1]) 
+            self.bp.exp(nram_buffer_in0[:calc_size], nram_buffer_in0[:calc_size], "hp")
+            self.bp.exp(nram_buffer_in1[:calc_size], nram_buffer_in1[:calc_size], "hp")
+            self.bp.add(nram_buffer_in0[:calc_size], nram_buffer_in0[:calc_size], nram_buffer_in1[:calc_size])
+            self.bp.log(nram_buffer_in0[:calc_size], nram_buffer_in0[:calc_size])
+            self.bp.memcpy(gram_buffer_out[once_loop_start:once_loop_end + 1], nram_buffer_in0[:calc_size])
       
-        out_res = self.bp.Buffer(#最终结果
-            shape=(data_calculated_each_time,),
-            name="OR",
-            dtype=self.dtype,
-            scope="nram",
-        )
-       
-        
-        with self.bp.for_range(0, loop_num) as i:   #正常循环       
-            start = task_id * data_calculated_each_task + i * data_calculated_each_time
-            stop = start + data_calculated_each_time
-            self.bp.memcpy(buffer_in0_n, buffer_in0[start:stop]) 
-            self.bp.memcpy(buffer_in1_n, buffer_in1[start:stop])         
-            self.bp.exp(natural_power_x,buffer_in0_n,"hp")
-            self.bp.exp(natural_power_y,buffer_in1_n,"hp")
-            self.bp.add(antilogarithm, natural_power_x, natural_power_y)
-            self.bp.log(out_res,antilogarithm)
-            self.bp.memcpy(buffer_out[start:stop], out_res)
-       
-        with self.bp.if_scope(tail_size > 0):# 当每个核有不够一次循环计算长度的  尾部补0 至一次计算的长度 单独算一次
-            # self.bp.print("length",self.length)
-            # self.bp.print("tail_zize",tail_size)
-            self.bp.zeros(buffer_in0_n)
-            self.bp.zeros(buffer_in1_n)
-            self.bp.zeros(natural_power_x)
-            self.bp.zeros(natural_power_y)
-            self.bp.zeros(antilogarithm)
-            self.bp.zeros(out_res)
-            start = (task_id)*data_calculated_each_task-loop_num*data_calculated_each_time
-            stop =start+ tail_size
-            self.bp.memcpy(buffer_in0_n[:tail_size], buffer_in0[start:stop]) 
-            self.bp.memcpy(buffer_in1_n[:tail_size], buffer_in1[start:stop])  
-            self.bp.exp(natural_power_x,buffer_in0_n,"hp")
-            self.bp.exp(natural_power_y,buffer_in1_n,"hp") 
-            self.bp.add(antilogarithm, natural_power_x, natural_power_y)
-            self.bp.log(out_res,antilogarithm)
-            self.bp.memcpy(buffer_out[start:stop], out_res[:tail_size])
-        # with self.bp.if_scope(insufficient_one_core_length>0):#当整体拆分任务至每个核时 如果有余  在这里计算  
-        #     self.bp.zeros(buffer_in0_n)
-        #     self.bp.zeros(buffer_in1_n)
-        #     self.bp.zeros(natural_power_x)
-        #     self.bp.zeros(natural_power_y)
-        #     self.bp.zeros(antilogarithm)
-        #     self.bp.zeros(out_res)
-        #     start = self.length - insufficient_one_core_length
-        #     stop = self.length
-        #     self.bp.memcpy(buffer_in0_n[:insufficient_one_core_length], buffer_in0[start:stop]) 
-        #     self.bp.memcpy(buffer_in1_n[:insufficient_one_core_length], buffer_in1[start:stop]) 
-        #     self.bp.exp(natural_power_x,buffer_in0_n,"hp")
-        #     self.bp.exp(natural_power_y,buffer_in1_n,"hp")
-        #     self.bp.add(antilogarithm, natural_power_x, natural_power_y)
-        #     self.bp.log(out_res,antilogarithm)
-        #     self.bp.memcpy(buffer_out[start:stop], out_res[:insufficient_one_core_length])
         f = self.bp.BuildBANG(
-            inputs=[buffer_in0, buffer_in1],
-            outputs=[buffer_out],
+            inputs=[gram_buffer_in0, gram_buffer_in0],
+            outputs=[gram_buffer_out],
             kernel_name=KERNEL_NAME,
         )
         return f
