defe8ba9def91b7e6a325517de6edff3d5f42645
mahxn0 <1262384588@qq.com>
2022-04-27 07:15:53 +0000

    feat(bangc-ops):Gtest Support Pb TestCases

--- bangc-ops/test/mlu_op_gtest/include/parser.h
+++ bangc-ops/test/mlu_op_gtest/include/parser.h
@@ -12,46 +12,54 @@
 #ifndef TEST_MLU_OP_GTEST_INCLUDE_PARSER_H_
 #define TEST_MLU_OP_GTEST_INCLUDE_PARSER_H_
 
-#include <google/protobuf/text_format.h>
-#include <google/protobuf/io/zero_copy_stream_impl.h>
-#include <google/protobuf/io/coded_stream.h>
-#include <string>
-#include <vector>
-#include <set>
-#include <algorithm>
-#include <sstream>
-#include <fstream>
-#include <iostream>
-#include "mlu_op_test.pb.h"
-#include "gtest/gtest.h"
-#include "mlu_op.h"
 #include "core/logging.h"
 #include "core/tensor.h"
 #include "core/type.h"
 #include "evaluator.h"
+#include "mlu_op.h"
+#include "mlu_op_test.pb.h"
 #include "tools.h"
+#include "gtest/gtest.h"
+#include <algorithm>
+#include <fstream>
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/text_format.h>
+#include <iostream>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
 
 namespace mluoptest {
 
 // value source
-enum ValueType { VALUE_F, VALUE_I, VALUE_L, VALUE_H, VALUE_RANDOM, VALUE_PATH, VALUE_INVALID };
+enum ValueType {
+  VALUE_F,
+  VALUE_I,
+  VALUE_L,
+  VALUE_H,
+  VALUE_RANDOM,
+  VALUE_PATH,
+  VALUE_INVALID
+};
 
 // only the tensor info saved in *pb.
 struct MetaTensor {
   std::string name = "unknown";
-  bool is_null     = false;  // null means desc and ptr both null;
+  bool is_null = false; // null means desc and ptr both null;
 
   // these size are for memory malloc
-  size_t shape_count = 0;  // not include stride.
+  size_t shape_count = 0; // not include stride.
   // count is 0, means 0 elements and ptr is null
   // but desc may not null.
-  size_t total_count   = 0;  // include stride_count
-  size_t size_in_bytes = 0;  // size_in_bytes = total_count * sizeof(mlu_dtype)
-  size_t sizeof_dtype  = 0;
+  size_t total_count = 0;   // include stride_count
+  size_t size_in_bytes = 0; // size_in_bytes = total_count * sizeof(mlu_dtype)
+  size_t sizeof_dtype = 0;
 
   // these for set to tensor desc
-  mluOpDataType_t dtype      = MLUOP_DTYPE_INVALID;
-  mluOpDataType_t oc_dt      = MLUOP_DTYPE_INVALID;
+  mluOpDataType_t dtype = MLUOP_DTYPE_INVALID;
+  mluOpDataType_t oc_dt = MLUOP_DTYPE_INVALID;
   mluOpTensorLayout_t layout = MLUOP_LAYOUT_ARRAY;
 
   // shape may not equal to total_count.
@@ -61,8 +69,8 @@ struct MetaTensor {
   std::vector<int> stride;
 
   int position = 0;
-  float scale  = 1.0;
-  int offset   = 0;
+  float scale = 1.0;
+  int offset = 0;
 
   ValueType value_type = VALUE_INVALID;
 
@@ -72,11 +80,11 @@ struct MetaTensor {
   // it's a not good idea to put *ptr and layout.. together.
   mluOpTensorDescriptor_t tensor = nullptr;
   // bool is_output       = false;
-  void *host_ptr       = nullptr;
-  void *dev_ptr        = nullptr;
-  void *dev_origin_ptr = nullptr;  // for origin device data
-  void *dev_perf_ptr   = nullptr;  // for perf test data
-  float *cpu_ptr       = nullptr;
+  void *host_ptr = nullptr;
+  void *dev_ptr = nullptr;
+  void *dev_origin_ptr = nullptr; // for origin device data
+  void *dev_perf_ptr = nullptr;   // for perf test data
+  float *cpu_ptr = nullptr;
   void print() {
     std::cout << "-=-=-=-=-=--=-=-=\n";
     std::cout << "tensor :" << name << " \n";
@@ -98,7 +106,7 @@ struct MetaTensor {
 };
 
 class Parser {
- public:
+public:
   Parser() {}
   virtual ~Parser();
   void parse(const std::string &file);
@@ -114,6 +122,7 @@ class Parser {
   // op params
   inline Node *node() { return proto_node_; }
   inline std::string getOpName() { return proto_node_->op_name(); }
+  void getTestInfo();
 
   // else
   inline Device device() { return device_; }
@@ -121,7 +130,10 @@ class Parser {
   bool common_threshold();
   bool check_threshold();
   inline std::set<Evaluator::Criterion> criterions() { return criterions_; }
-  std::set<Evaluator::Criterion> criterions(int index, std::vector<int> criterions_use);
+  std::set<Evaluator::Criterion> criterions(int index,
+                                            std::vector<int> criterions_use);
+  std::vector<std::string> getBlOfZeroInput() { return bl_zeroinput_; }
+  std::vector<std::string> getBlOfMluOnlyFast() { return bl_mlu_only_fast_; }
 
   MetaTensor &getMetaTensor(const std::string &name);
   MetaTensor &getMetaTensor(int index);
@@ -132,54 +144,88 @@ class Parser {
   inline float getInputScale(int index) { return inputs_.at(index).scale; }
   inline float getOutputScale(int index) { return outputs_.at(index).scale; }
   inline int getInputPosition(int index) { return inputs_.at(index).position; }
-  inline int getOutputPosition(int index) { return outputs_.at(index).position; }
+  inline int getOutputPosition(int index) {
+    return outputs_.at(index).position;
+  }
   inline int getInputOffset(int index) { return inputs_.at(index).offset; }
   inline int getOutputOffset(int index) { return outputs_.at(index).offset; }
-  inline mluOpDataType_t getInputDataType(int index) { return inputs_.at(index).dtype; }
-  inline mluOpDataType_t getOutputDataType(int index) { return outputs_.at(index).dtype; }
-  inline mluOpDataType_t getInputOnchipDataType(int index) { return inputs_.at(index).oc_dt; }
-  inline mluOpDataType_t getOutputOnchipDataType(int index) { return outputs_.at(index).oc_dt; }
-  inline mluOpTensorLayout_t getInputLayout(int index) { return inputs_.at(index).layout; }
-  inline mluOpTensorLayout_t getOutputLayout(int index) { return outputs_.at(index).layout; }
-  inline size_t getInputDataCount(int index) { return inputs_.at(index).shape_count; }
-  inline size_t getOutputDataCount(int index) { return outputs_.at(index).shape_count; }
-  inline int getInputDimSize(int index) { return inputs_.at(index).shape.size(); }
-  inline int getOutputDimSize(int index) { return outputs_.at(index).shape.size(); }
-  inline int getInputDimStrideSize(int index) { return inputs_.at(index).stride.size(); }
-  inline int getOutputDimStrideSize(int index) { return outputs_.at(index).stride.size(); }
+  inline mluOpDataType_t getInputDataType(int index) {
+    return inputs_.at(index).dtype;
+  }
+  inline mluOpDataType_t getOutputDataType(int index) {
+    return outputs_.at(index).dtype;
+  }
+  inline mluOpDataType_t getInputOnchipDataType(int index) {
+    return inputs_.at(index).oc_dt;
+  }
+  inline mluOpDataType_t getOutputOnchipDataType(int index) {
+    return outputs_.at(index).oc_dt;
+  }
+  inline mluOpTensorLayout_t getInputLayout(int index) {
+    return inputs_.at(index).layout;
+  }
+  inline mluOpTensorLayout_t getOutputLayout(int index) {
+    return outputs_.at(index).layout;
+  }
+  inline size_t getInputDataCount(int index) {
+    return inputs_.at(index).shape_count;
+  }
+  inline size_t getOutputDataCount(int index) {
+    return outputs_.at(index).shape_count;
+  }
+  inline int getInputDimSize(int index) {
+    return inputs_.at(index).shape.size();
+  }
+  inline int getOutputDimSize(int index) {
+    return outputs_.at(index).shape.size();
+  }
+  inline int getInputDimStrideSize(int index) {
+    return inputs_.at(index).stride.size();
+  }
+  inline int getOutputDimStrideSize(int index) {
+    return outputs_.at(index).stride.size();
+  }
   inline void getInputData(int index, void *data) {
     getInputTensorValue(index, data, inputs_[index].total_count);
   }
   inline void getOutputData(int index, void *data) {
     getOutputTensorValue(index, data, outputs_[index].total_count);
   }
-  inline void getInputDims(int index, int dim_size, int *dim_array, int *dim_stride = nullptr) {
+  inline void getInputDims(int index, int dim_size, int *dim_array,
+                           int *dim_stride = nullptr) {
     auto ts = inputs_.at(index);
     memcpy(dim_array, ts.shape.data(), ts.shape.size() * sizeof(int));
     if (dim_stride) {
       memcpy(dim_stride, ts.stride.data(), ts.stride.size() * sizeof(int));
     }
   }
-  inline void getOutputDims(int index, int dim_size, int *dim_array, int *dim_stride = nullptr) {
+  inline void getOutputDims(int index, int dim_size, int *dim_array,
+                            int *dim_stride = nullptr) {
     auto ts = outputs_.at(index);
     memcpy(dim_array, ts.shape.data(), ts.shape.size() * sizeof(int));
     if (dim_stride) {
       memcpy(dim_stride, ts.stride.data(), ts.stride.size() * sizeof(int));
     }
   }
-  inline ValueType getInputValueType(int index) { return inputs_.at(index).value_type; }
+  inline ValueType getInputValueType(int index) {
+    return inputs_.at(index).value_type;
+  }
   inline Node *getProtoNode() { return proto_node_; }
 
- private:
+private:
   Node *proto_node_ = nullptr;
   std::vector<MetaTensor> inputs_;
   std::vector<MetaTensor> outputs_;
   std::set<Evaluator::Criterion> criterions_;
   std::string op_name_;
+  std::string pb_path_;
+  std::vector<std::string> bl_zeroinput_;
+  std::vector<std::string> bl_mlu_only_fast_;
   Device device_ = CPU;
 
   ValueType getValueType(const Tensor *t);
-  void getTensorValue(Tensor *pt, void *data, ValueType value_type, size_t count);
+  void getTensorValue(Tensor *pt, void *data, ValueType value_type,
+                      size_t count);
   void getTensorValueH(Tensor *pt, void *data, size_t count);
   void getTensorValueF(const Tensor *pt, void *data, size_t count);
   void getTensorValueI(const Tensor *pt, void *data, size_t count);
@@ -199,5 +245,5 @@ class Parser {
   bool readMessageFromFile(const std::string &filename, Node *proto);
 };
 
-}  // namespace mluoptest
-#endif  // TEST_MLU_OP_GTEST_INCLUDE_PARSER_H_
+} // namespace mluoptest
+#endif // TEST_MLU_OP_GTEST_INCLUDE_PARSER_H_
