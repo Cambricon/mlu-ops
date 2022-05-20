f7021f5495b13cfca391b619c0a723dee0a52453
SS7D631 <121872426@qq.com>
2022-05-20 11:06:22 +0800

    logaddexp 已全部通过pylint  仅剩celu函数拆分

--- bangpy-ops/ops/logaddexp/test_logaddexp.py
+++ bangpy-ops/ops/logaddexp/test_logaddexp.py
@@ -20,22 +20,58 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # pylint: disable=missing-docstring, invalid-name, too-many-locals
 """A multi-platform code link example test for BANGPy TCP."""
-import sys
 import random
 import numpy as np
 import pytest
-import bangpy
 import bangpy as bp
 from bangpy import tcp
 from bangpy.common import load_op_by_type
 from bangpy.platform.bang_config import TARGET
 from bangpy.tcp.runtime import TaskType
 from logaddexp import DTYPES,TARGET_LIST
-sys.path.append("..")
-from create_shape import *
-
-
-test_shape_list = CreatShapeList(
+# 生成随机元组
+def random_int_list(max_dim_length, each_dim_max_length):
+    random_list = []
+    for _ in range(max_dim_length):
+        random_list.append(random.randint(2, each_dim_max_length))
+    return tuple(random_list)
+# nram_single_buffer_size_by_byte 核上单个buffer得空间单位字节
+# append_test_count 随机生成shape的个数
+# max_dim_length 最大维度数
+# each_dim_max_length 每个维度最大多少个元素
+def CreatShapeList(
+    nram_single_buffer_size_by_byte, append_test_count=50, max_dim_length=5, each_dim_max_length=64
+):
+    const_float32_128_align_element_count = 32  # float32 下 128字节对应元素个数
+    const_float16_128_align_element_count = 64  # float16 下 128字节对应元素个数
+    const_current_mlu_single_buffer_float32_max_element_size = int(
+        nram_single_buffer_size_by_byte / 4)  # float32下 单个nram_buffer的最大元素数
+    const_current_mlu_single_buffer_float16_max_element_size = int(
+        nram_single_buffer_size_by_byte / 2)  # float16下 单个nram_buffer的最大元素数
+    # 内置固定检测shape
+    test_shape_list = [
+        (1,),
+        (2,),
+        # 128字节对齐边界测试
+        (const_float32_128_align_element_count - 1,),  # 不足128对齐
+        (const_float32_128_align_element_count,),  # 满足128对齐
+        (const_float32_128_align_element_count + 1,),  # 128对齐后多1个
+        (const_float16_128_align_element_count - 1,),
+        (const_float16_128_align_element_count,),
+        (const_float16_128_align_element_count + 1,),
+        # nram_buffer边界测试
+        (const_current_mlu_single_buffer_float32_max_element_size - 1,),  # 比空间大小少一个元素
+        (const_current_mlu_single_buffer_float32_max_element_size,),  # 刚好用完空间
+        (const_current_mlu_single_buffer_float32_max_element_size + 1,),  # 比空间大小多一个元素
+        (const_current_mlu_single_buffer_float16_max_element_size - 1,),
+        (const_current_mlu_single_buffer_float16_max_element_size,),
+        (const_current_mlu_single_buffer_float16_max_element_size + 1,),
+    ]
+    for _ in range(append_test_count):
+        test_shape_list.append(random_int_list(random.randint(
+            2, max_dim_length), random.randint(2, each_dim_max_length)))
+    return test_shape_list
+shape_list = CreatShapeList(
     nram_single_buffer_size_by_byte = (512 - 40) * 1024 // 8,
     append_test_count = 50,
     max_dim_length = 5,
@@ -43,7 +79,7 @@ test_shape_list = CreatShapeList(
 )
 @pytest.mark.parametrize(
     "shape",
-    test_shape_list,
+    shape_list,
     #[(65,),]
 )
 @pytest.mark.parametrize(
@@ -101,11 +137,10 @@ def test_logaddexp(target, shape, dtype):
             with tcp.runtime.Run(task_type):
                 log_add_exp_func(x, y, output_dev)
             return output_dev.numpy().reshape(max_size_buffer.shape)
-        else :
-            print("need the same shape or sub shape")
+        return 0
     mlu_ret = logaddexp(data_x,data_y)
     ret2 = np.logaddexp(data_x, data_y)
     if dtype.name == "float16":
-        bangpy.assert_allclose(mlu_ret, ret2.astype(dtype.as_numpy_dtype),rtol = 0.01, atol = 0.01)
+        bp.assert_allclose(mlu_ret, ret2.astype(dtype.as_numpy_dtype),rtol = 0.01, atol = 0.01)
     else:
-        bangpy.assert_allclose(mlu_ret, ret2.astype(dtype.as_numpy_dtype),rtol = 0.001, atol = 0.01)
+        bp.assert_allclose(mlu_ret, ret2.astype(dtype.as_numpy_dtype),rtol = 0.001, atol = 0.01)
