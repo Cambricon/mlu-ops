3002d215601ca4327338f55e66b8aa08b62c5795
guanxindi <gj@qq.com>
2022-05-20 13:09:35 +0800

    dataman恢复原先的结构

--- bangpy-ops/ops/pairwise_distance/pairwise_distance.py
+++ bangpy-ops/ops/pairwise_distance/pairwise_distance.py
@@ -30,37 +30,9 @@ DTYPES = [bangpy.float32] #支持的类型
 TARGET_LIST = ["mlu290"]#支持的设备
 KERNEL_NAME = "PairwiseDistance"#算子名
 
-class data_man:
-    bp = None
-
-    m_current_core_start = None
-    m_current_core_end = None
-    m_total_count_in_core = None
-
-    def init(self, bp):
-        self.bp = bp
-        self.m_current_core_start = self.bp.Scalar(bangpy.int32, "current_core_start")
-        self.m_current_core_end = self.bp.Scalar(bangpy.int32, "current_core_end")
-        self.m_total_count_in_core = self.bp.Scalar(bangpy.int32, "total_count_in_core")
-
-    def calc_core_process_count(self, data_total_len, task_num):
-        one_core_count = self.bp.Scalar(bangpy.int32, "one_core_count")
-        one_core_count.assign(data_total_len // task_num)
-
-        remain = self.bp.Scalar(bangpy.int32, "remain")
-        remain.assign(data_total_len % task_num)
-
-        with self.bp.if_scope(self.bp.taskId < remain): #如果存在余数 将其均摊给各核   taskId从0起
-            self.m_current_core_start.assign((one_core_count + 1) * self.bp.taskId )
-            self.m_current_core_end.assign((one_core_count + 1) * \
-                (self.bp.taskId + 1) - 1) #此处应该不需要减1 待验证  python切片会自动将上标减1
-        with self.bp.else_scope():
-            self.m_current_core_start.assign((one_core_count + 1) * \
-                remain + one_core_count * (self.bp.taskId - remain))
-            self.m_current_core_end.assign((one_core_count + 1) * remain + \
-                one_core_count * (self.bp.taskId - remain) + one_core_count - 1)
-
-        self.m_total_count_in_core.assign(self.m_current_core_end - self.m_current_core_start + 1)
+import sys
+sys.path.append("..")
+from data_man import *
 
 dman = data_man()
 
@@ -73,12 +45,6 @@ class pd_para:
     pd_width = None
 
     output_len = None
-
-    def run(self):
-        pass
-
-    def run2(self):
-        pass
 
 
 class PairwiseDistance:
