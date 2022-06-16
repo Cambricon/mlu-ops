4155489f9eed7f90b5f6d5a4c1241a41b8ff4494
zcyKTH <zcy19950525@gmail.com>
2022-06-16 11:55:08 +0800

    [Refactor] refactor the format of mlu_op.h

--- bangc-ops/mlu_op.h
+++ bangc-ops/mlu_op.h
@@ -36,14 +36,14 @@ extern "C" {
  *
  */
 typedef enum {
-  MLUOP_LOG_E  = 0, /*!< The base e is used.*/
-  MLUOP_LOG_2  = 1, /*!< The base 2 is used.*/
+  MLUOP_LOG_E = 0,  /*!< The base e is used.*/
+  MLUOP_LOG_2 = 1,  /*!< The base 2 is used.*/
   MLUOP_LOG_10 = 2, /*!< The base 10 is used.*/
 } mluOpLogBase_t;
 
 /*!
- * @brief Computes the absolute value for every element of the input tensor \b x and returns in \b
- y.
+ * @brief Computes the absolute value for every element of the input tensor \b x
+ * and returns in \b y.
  *
  * @param[in] handle
  *   Input. Handle to a MLUOP context that is used to manage MLU devices and
@@ -91,33 +91,31 @@ typedef enum {
  * - https://www.tensorflow.org/api_docs/python/tf/math/abs
  */
 mluOpStatus_t MLUOP_WIN_API mluOpAbs(mluOpHandle_t handle,
-                                  const mluOpTensorDescriptor_t x_desc,
-                                  const void *x,
-                                  const mluOpTensorDescriptor_t y_desc,
-                                  void *y);
+                                     const mluOpTensorDescriptor_t x_desc,
+                                     const void *x,
+                                     const mluOpTensorDescriptor_t y_desc,
+                                     void *y);
 
 /*!
- * @brief Computes logarithm of input tensor \b x, and returns the results in the output tensor \b
- * y.
- *
- * Compared with ::mluOpLog, this function allows you to choose whether to perform log operation
- * with faster algorithm or higher precision.
+ * @brief Computes logarithm of input tensor \b x, and returns the results in
+ * the output tensor \b y.
  *
  * @param[in] handle
- *   Input. Handle to a MLUOP context that is used to manage MLU devices and queues in the log
- *   operation. For detailed information, see ::mluOpHandle_t.
+ *   Input. Handle to a MLUOP context that is used to manage MLU devices and
+ *   queues in the log operation. For detailed information, see ::mluOpHandle_t.
  * @param[in] prefer
  *   Input. The \b prefer modes defined in ::mluOpComputationPreference_t enum.
  * @param[in] base
- *    Input. A mluOpLogBase_t type value indicating which base (e, 2 or 10) to be used.
+ *   Input. A mluOpLogBase_t type value indicating which base (e, 2 or 10) to
+ *   be used.
  * @param[in] x_desc
  *   Input. The descriptor of the input tensor. For detailed information, see
- * ::mluOpTensorDescriptor_t.
+ *   ::mluOpTensorDescriptor_t.
  * @param[in] x
  *   Input. Pointer to the MLU memory that stores the input tensor \b x.
  * @param[in] y_desc
  *   Input. The descriptor of the output tensor. For detailed information, see
- * ::mluOpTensorDescriptor_t.
+ *   ::mluOpTensorDescriptor_t.
  * @param[out] y
  *   Output. Pointer to the MLU memory that stores the output tensor \b y.
  *
@@ -134,8 +132,8 @@ mluOpStatus_t MLUOP_WIN_API mluOpAbs(mluOpHandle_t handle,
  *   - output tensor: half, float.
  *
  * @par Scale Limitation
- * - The input tensor and output tensor have the same shape, and the input tensor must meet
- *   the following input data range:
+ * - The input tensor and output tensor have the same shape, and the input
+ *   tensor must meet the following input data range:
  *   - float: [1e-20, 2e5].
  *   - half: [1, 60000].
  *
@@ -151,39 +149,34 @@ mluOpStatus_t MLUOP_WIN_API mluOpAbs(mluOpHandle_t handle,
  * @par Reference
  * - https://www.tensorflow.org/api_docs/python/tf/math/log
  */
-mluOpStatus_t MLUOP_WIN_API mluOpLog(mluOpHandle_t handle,
-                                  const mluOpComputationPreference_t prefer,
-                                  const mluOpLogBase_t base,
-                                  const mluOpTensorDescriptor_t x_desc,
-                                  const void *x,
-                                  const mluOpTensorDescriptor_t y_desc,
-                                  void *y);
+mluOpStatus_t MLUOP_WIN_API
+mluOpLog(mluOpHandle_t handle, const mluOpComputationPreference_t prefer,
+         const mluOpLogBase_t base, const mluOpTensorDescriptor_t x_desc,
+         const void *x, const mluOpTensorDescriptor_t y_desc, void *y);
 
 /*!
- * @brief Computes division on input tensor \b x and \b y, and returns the results
- *        in the output tensor \b output.
- *
- * Compared with ::mluOpDiv, this function allows you to choose whether to perform div
- * operation with faster algorithm or higher precision.
+ * @brief Computes division on input tensor \b x and \b y, and returns the
+ * results in the output tensor \b output.
  *
  * @param[in] handle
- *   Input. Handle to a MLUOP context that is used to manage MLU devices and queues in the division
- *   operation. For detailed information, see ::mluOpHandle_t.
+ *   Input. Handle to a MLUOP context that is used to manage MLU devices and
+ *   queues in the division operation. For detailed information, see
+ *   ::mluOpHandle_t.
  * @param[in] prefer
  *   Input. The \b prefer modes defined in ::mluOpComputationPreference_t enum.
  * @param[in] x_desc
  *   Input. The descriptor of the input tensor. For detailed information, see
- * ::mluOpTensorDescriptor_t.
+ *   ::mluOpTensorDescriptor_t.
  * @param[in] x
  *   Input. Pointer to the MLU memory that stores the dividend tensor.
  * @param[in] y_desc
  *   Input. The descriptor of the input tensor. For detailed information, see
- * ::mluOpTensorDescriptor_t.
+ *   ::mluOpTensorDescriptor_t.
  * @param[in] y
  *   Input. Pointer to the MLU memory that stores the divisor tensor.
  * @param[in] z_desc
  *   Input. The descriptor of the output tensor. For detailed information, see
- * ::mluOpTensorDescriptor_t.
+ *   ::mluOpTensorDescriptor_t.
  * @param[out] z
  *   Output. Pointer to the MLU memory that stores the output tensor.
  *
@@ -203,10 +196,10 @@ mluOpStatus_t MLUOP_WIN_API mluOpLog(mluOpHandle_t handle,
  * - The input tensors and output tensor must have the same shape.
  *
  * @note
- * - The inputs \b x and \b y are multi-dimensional array, supporting up to MLUOP_DIM_MAX dimensions.
- * - When input \b y data type is float, \b y data range is [-1e10,-1e-20] & [1e-20,1e10]. When \b y
- * data type is
- *   half, \b y data range is [-65504,-1e-4] & [1e-4,65504].
+ * - The input tensors and output tensor have the same shape, and the input
+ *   tensor \b y must meet the following input data range:
+ *   - float: [-1e10,-1e-20] & [1e-20,1e10].
+ *   - half: [-65504,-1e-4] & [1e-4,65504].
  *
  * @par Requirements
  * - None.
@@ -217,34 +210,30 @@ mluOpStatus_t MLUOP_WIN_API mluOpLog(mluOpHandle_t handle,
  * @par Reference
  * - https://www.tensorflow.org/api_docs/python/tf/math/divide
  */
-mluOpStatus_t MLUOP_WIN_API mluOpDiv(mluOpHandle_t handle,
-                                  const mluOpComputationPreference_t prefer,
-                                  const mluOpTensorDescriptor_t x_desc,
-                                  const void *x,
-                                  const mluOpTensorDescriptor_t y_desc,
-                                  const void *y,
-                                  const mluOpTensorDescriptor_t z_desc,
-                                  void *z);
+mluOpStatus_t MLUOP_WIN_API
+mluOpDiv(mluOpHandle_t handle, const mluOpComputationPreference_t prefer,
+         const mluOpTensorDescriptor_t x_desc, const void *x,
+         const mluOpTensorDescriptor_t y_desc, const void *y,
+         const mluOpTensorDescriptor_t z_desc, void *z);
 
 /*!
- * @brief Computes sqrt on input tensor \b x, and returns the results in the output tensor \b y.
- *
- * This function allows you to choose whether to perform sqrt operation with faster algorithm or
- * higher precision.
+ * @brief Computes sqrt on input tensor \b x, and returns the results in the
+ * output tensor \b y.
  *
  * @param[in] handle
- *   Input. Handle to a MLUOP context that is used to manage MLU devices and queues in the sqrt
- *   operation. For detailed information, see ::mluOpHandle_t.
+ *   Input. Handle to a MLUOP context that is used to manage MLU devices and
+ *   queues in the sqrt operation. For detailed information, see
+ *   ::mluOpHandle_t.
  * @param[in] prefer
  *   Input. The \b prefer modes defined in ::mluOpComputationPreference_t enum.
  * @param[in] x_desc
  *   Input. The descriptor of the input tensor. For detailed information, see
- ::mluOpTensorDescriptor_t.
+ *   ::mluOpTensorDescriptor_t.
  * @param[in] x
  *   Input. Pointer to the MLU memory that stores the input tensor.
  * @param[in] y_desc
  *   Input. The descriptor of the output tensor. For detailed information, see
- ::mluOpTensorDescriptor_t.
+ *   ::mluOpTensorDescriptor_t.
  * @param[out] y
  *   Output. Pointer to the MLU memory that stores the output tensor.
  *
@@ -261,8 +250,8 @@ mluOpStatus_t MLUOP_WIN_API mluOpDiv(mluOpHandle_t handle,
  *   - output tensor: half, float.
  *
  * @par Scale Limitation
- * - The input tensor and output tensor must have the same shape, and the input tensor must meet
- *   the following input data range:
+ * - The input tensor and output tensor must have the same shape, and the input
+ *   tensor must meet the following input data range:
  *   - float: [1e-10,1e10].
  *   - half: [1e-3,1e-2] & [1e-1,60000].
  *
@@ -271,35 +260,38 @@ mluOpStatus_t MLUOP_WIN_API mluOpDiv(mluOpHandle_t handle,
  *
  * @par Example
  * - None.
-
+ *
  * @par Reference
  * - https://www.tensorflow.org/api_docs/python/tf/math/sqrt
  */
 mluOpStatus_t MLUOP_WIN_API mluOpSqrt(mluOpHandle_t handle,
-                                   const mluOpComputationPreference_t prefer,
-                                   const mluOpTensorDescriptor_t x_desc,
-                                   const void *x,
-                                   const mluOpTensorDescriptor_t y_desc,
-                                   void *y);
+                                      const mluOpComputationPreference_t prefer,
+                                      const mluOpTensorDescriptor_t x_desc,
+                                      const void *x,
+                                      const mluOpTensorDescriptor_t y_desc,
+                                      void *y);
 
 /*!
- * @brief Computes gradient of sqrt on input tensor \b y and \b diff_y, and returns the results
- *        in the output tensor \b diff_x.
+ * @brief Computes gradient of sqrt on input tensor \b y and \b diff_y, and
+ * returns the results in the output tensor \b diff_x.
  *
  * @param[in] handle
- *   Input. Handle to a MLUOP context that is used to manage MLU devices and queues in the sqrt
- * backward
- *   operation. For detailed information, see ::mluOpHandle_t.
+ *   Input. Handle to a MLUOP context that is used to manage MLU devices and
+ *   queues in the sqrt backward operation. For detailed information, see
+ *   ::mluOpHandle_t.
  * @param[in] y_desc
- *   Input. The descriptor of the tensors. For detailed information, see ::mluOpTensorDescriptor_t.
+ *   Input. The descriptor of the tensors. For detailed information, see
+ *   ::mluOpTensorDescriptor_t.
  * @param[in] y
  *   Input. Pointer to the MLU memory that stores the input tensor.
  * @param[in] dy_desc
- *   Input. The descriptor of the tensors. For detailed information, see ::mluOpTensorDescriptor_t.
+ *   Input. The descriptor of the tensors. For detailed information, see
+ *   ::mluOpTensorDescriptor_t.
  * @param[in] diff_y
  *   Input. Pointer to the MLU memory that stores the input tensor.
  * @param[in] dx_desc
- *   Input. The descriptor of the tensors. For detailed information, see ::mluOpTensorDescriptor_t.
+ *   Input. The descriptor of the tensors. For detailed information, see
+ *   ::mluOpTensorDescriptor_t.
  * @param[out] diff_x
  *   Output. Pointer to the MLU memory that stores the output tensor.
  *
@@ -307,7 +299,8 @@ mluOpStatus_t MLUOP_WIN_API mluOpSqrt(mluOpHandle_t handle,
  * - ::MLUOP_STATUS_SUCCESS, ::MLUOP_STATUS_BAD_PARAM
  *
  * @par Formula
- * - See "Sqrt Backward Operation" section in "Cambricon MLUOP User Guide" for details.
+ * - See "Sqrt Backward Operation" section in "Cambricon MLUOP User Guide" for
+ * details.
  *
  * @par Data Type
  * - Data types of input tensors and output tensor must be the same.
@@ -316,9 +309,8 @@ mluOpStatus_t MLUOP_WIN_API mluOpSqrt(mluOpHandle_t handle,
  *   - output tensor: half, float.
  *
  * @par Scale Limitation
- * - The input tensors and output tensor must have the same shape, and the input tensor \b y must
- * meet
- *   the following input data range:
+ * - The input tensors and output tensor must have the same shape, and the input
+ *   tensor \b y must meet the following input data range:
  *   - float: [1e-10,1e6].
  *   - half: [0.01,500].
  *
@@ -331,13 +323,10 @@ mluOpStatus_t MLUOP_WIN_API mluOpSqrt(mluOpHandle_t handle,
  * @par Reference
  * - https://www.tensorflow.org/api_docs/python/tf/raw_ops/SqrtGrad
  */
-mluOpStatus_t MLUOP_WIN_API mluOpSqrtBackward(mluOpHandle_t handle,
-                                           const mluOpTensorDescriptor_t y_desc,
-                                           const void *y,
-                                           const mluOpTensorDescriptor_t dy_desc,
-                                           const void *diff_y,
-                                           const mluOpTensorDescriptor_t dx_desc,
-                                           void *diff_x);
+mluOpStatus_t MLUOP_WIN_API mluOpSqrtBackward(
+    mluOpHandle_t handle, const mluOpTensorDescriptor_t y_desc, const void *y,
+    const mluOpTensorDescriptor_t dy_desc, const void *diff_y,
+    const mluOpTensorDescriptor_t dx_desc, void *diff_x);
 
 #if defined(__cplusplus)
 }
