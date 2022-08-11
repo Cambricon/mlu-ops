cf2d4e346faa5d44ffd12d16ef1758cb935a0fb3
liuduanhui <103939338+DanieeelLiu@users.noreply.github.com>
2022-08-11 15:45:59 +0800

    feat(bangc-ops): support user defined NEUWARE_HOME. (#120)

--- env.sh
+++ env.sh
@@ -3,7 +3,14 @@ export BANGC_HOME=${SCRIPT_PATH}/bangc-ops
 export BANGPY_HOME=${SCRIPT_PATH}/bangpy-ops
 export BANGPY_BUILD_PATH=${BANGPY_HOME}/outs
 export CPLUS_INCLUDE_PATH=${BANGPY_HOME}/include/:${CPLUS_INCLUDE_PATH}
-export NEUWARE_HOME=/usr/local/neuware
+
+if [[ -z ${NEUWARE_HOME} ]]; then
+  export NEUWARE_HOME=/usr/local/neuware
+  echo "set env 'NEUWRAE_HOME' to default: ${NEUWARE_HOME}."
+else
+  echo "env 'NEUWRAE_HOME': ${NEUWARE_HOME}."
+fi
+
 export PATH=${NEUWARE_HOME}/bin:${PATH}
 export LD_LIBRARY_PATH=${NEUWARE_HOME}/lib64:${BANGC_HOME}/build/lib:${LD_LIBRARY_PATH}
 
