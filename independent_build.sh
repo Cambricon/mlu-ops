c8bc4e1221d5456afc2612cb8b5943f6fc321bcb
liuduanhui <103939338+DanieeelLiu@users.noreply.github.com>
2022-09-21 21:12:50 +0800

    [Fix](bangc-ops): fix test tar configuration. (#215)

--- independent_build.sh
+++ independent_build.sh
@@ -155,6 +155,7 @@ fi
 echo "[independent_build.sh] bangc-ops-build success"
 
 BUILD_DIR="bangc-ops/build"
+BANGCOPS_DIR="bangc-ops"
 PACKAGE_DIR="package/usr/local/neuware"
 mkdir -p ${PACKAGE_DIR}
 mkdir -p ${PACKAGE_DIR}/include
@@ -166,10 +167,12 @@ cp bangc-ops/mlu_op.h bangc-ops/mlu_op_kernel.h ${PACKAGE_DIR}/include
 TEST_DIR="test_workspace/mluops"
 mkdir -p ${TEST_DIR}/build
 mkdir -p ${TEST_DIR}/lib
+mkdir -p ${TEST_DIR}/test
 
 cp -rf ${BUILD_DIR}/test ${TEST_DIR}/build
 cp -rf ${BUILD_DIR}/lib/libgtest_shared.a ${TEST_DIR}/lib
 cp -rf ${BUILD_DIR}/lib/libmluop_test_proto.a ${TEST_DIR}/lib
+cp -rf ${BANGCOPS_DIR}/test/* ${TEST_DIR}/test
 
 DEPS_DIR=`echo ${PACKAGE_SERVER} | awk -F '//' '{print $2}'`
 rm -rf $DEPS_DIR
