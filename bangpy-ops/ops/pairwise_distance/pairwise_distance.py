ceed60165926ba7d14be8f8a3250b3a81a354af6
unknown <1422749473@qq.com>
2022-10-02 00:07:09 +0800

    准备添加pd

---
+++ bangpy-ops/ops/pairwise_distance/pairwise_distance.py
@@ -0,0 +1,87 @@
+# Copyright (C) [2021] by Cambricon, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# pylint: disable=missing-docstring, invalid-name, too-many-locals
+"""A multi-platform code link example test for BANGPy TCP."""
+import bangpy
+from bangpy import tcp
+from bangpy.script import ty, build_module
+
+
+DTYPES = [bangpy.float16, bangpy.float32]
+TARGET_LIST = ["mlu370-s4"]
+KERNEL_NAME = "pairwise_distance"
+
+
+class PairwiseDistance(object):
+    """Operator description:
+    Add the data in the two buffers.
+    """
+    def __init__(self, buffer_size: ty.int32, dtype: ty.string) -> None:
+        self.dtype = dtype
+        self.single_buffer_size = buffer_size
+
+    def add_body(
+        self,
+        local_a: ty.Buffer("nram"),  # type: ignore
+        local_b: ty.Buffer("nram"),  # type: ignore
+        local_c: ty.Buffer("nram"),  # type: ignore
+    ) -> None:
+        # The body of add function
+        tcp.add(local_a, local_b, local_c)
+
+    def main(self, a: ty.handle, b: ty.handle, c: ty.handle, length: ty.int32) -> None:
+        A = tcp.match_buffer(a, [length], dtype=self.dtype)
+        B = tcp.match_buffer(b, [length], dtype=self.dtype)
+        C = tcp.match_buffer(c, [length], dtype=self.dtype)
+        tgt = tcp.target()
+        # calculate split strategy
+        # gets the data length to be calculated for each task
+        data_calculated_each_task = length // (tgt.cluster_num * tgt.core_num)
+        # gets the number of cycles required for each task
+        loop_num = data_calculated_each_task // self.single_buffer_size
+
+        buffer_in0 = tcp.alloc_buffer(
+            [self.single_buffer_size], dtype=self.dtype, scope="nram"
+        )
+        buffer_in1 = tcp.alloc_buffer(
+            [self.single_buffer_size], dtype=self.dtype, scope="nram"
+        )
+        buffer_out = tcp.alloc_buffer(
+            [self.single_buffer_size], dtype=self.dtype, scope="nram"
+        )
+        for cluster_id in tcp.thread_binding(0, tgt.cluster_num, thread="blockIdx.x"):
+            for core_id in tcp.thread_binding(0, tgt.core_num, thread="threadIdx.x"):
+                for i in range(loop_num):
+                    task_id = cluster_id * tgt.core_num + core_id
+                    start = task_id * data_calculated_each_task + i * self.single_buffer_size
+                    stop = start + self.single_buffer_size
+                    tcp.memcpy(buffer_in0, A[start:stop])
+                    tcp.memcpy(buffer_in1, B[start:stop])
+                    self.add_body(buffer_out, buffer_in0, buffer_in1)
+                    tcp.memcpy(C[start:stop], buffer_out)
+
+
+@tcp.register_mlu_op(DTYPES, TARGET_LIST, KERNEL_NAME)
+def build_add(dtype=None, target=None):
+    f = build_module.build(
+        PairwiseDistance(64, dtype.name), target_tag=target, name=KERNEL_NAME
+    )
+    return f
---
+++ bangpy-ops/ops/pairwise_distance/test_pairwise_distance.py
@@ -0,0 +1,52 @@
+# Copyright (C) [2021] by Cambricon, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# pylint: disable=missing-docstring, invalid-name, too-many-locals
+"""A multi-platform code link example test for BANGPy TCP."""
+import numpy as np
+import pytest
+import bangpy
+from bangpy.common import load_op_by_type
+from pairwise_distance import DTYPES, KERNEL_NAME, TARGET_LIST
+
+
+@pytest.mark.parametrize(
+    "shape", [(4096,), (8192,),],
+)
+@pytest.mark.parametrize(
+    "dtype", DTYPES,
+)
+def test_pairwise_distance(target, shape, dtype):
+    if target not in TARGET_LIST:
+        return
+    data_in0 = np.random.uniform(low=-10, high=10, size=shape)
+    data_in1 = np.random.uniform(low=-10, high=10, size=shape)
+
+    data_out = data_in0.astype(dtype.as_numpy_dtype) + data_in1.astype(
+        dtype.as_numpy_dtype
+    )
+    dev = bangpy.device(0)
+    # set I/O data
+    data_in0_dev = bangpy.Array(data_in0.astype(dtype.as_numpy_dtype), dev)
+    data_in1_dev = bangpy.Array(data_in1.astype(dtype.as_numpy_dtype), dev)
+    data_out_dev = bangpy.Array(np.zeros(data_out.shape, dtype.as_numpy_dtype), dev)
+    f1 = load_op_by_type(KERNEL_NAME, dtype.name)
+    f1(data_in0_dev, data_in1_dev, data_out_dev, data_in0.shape[0])
+    bangpy.assert_allclose(data_out_dev.numpy(), data_out.astype(dtype.as_numpy_dtype))
