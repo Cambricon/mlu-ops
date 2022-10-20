f621692d7b9353543b84f25e8c547679632e5913
wenzhengyin <71548662+Jones154@users.noreply.github.com>
2022-10-20 13:16:33 +0800

    Update ml_nms.cpp

--- bangc-ops/kernels/ml_nms/ml_nms.cpp
+++ bangc-ops/kernels/ml_nms/ml_nms.cpp
@@ -1,20 +1,28 @@
 /*************************************************************************
-	> File Name: main.cpp
-	> Author: wenzhengyin
-	> Mail: jones980116@163.com 
-	> Created Time: Tue Apr 19 14:35:06 2022
- ************************************************************************/
-#include "cnrt.h"
-#include "cndev.h"
-//#include "cnrt_data.h"
+* Copyright (C) [2022] by Cambricon, Inc.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+************************************************************************/
 #include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include "core/mlu_op_core.h"
+//#include "core/mlu_op_core.h"
+#include "mlu_op.h"
 #include "core/context.h"
 #include "core/gen_case.h"
 #include "core/logging.h"
@@ -22,53 +30,92 @@
 #include "core/tensor.h"
 #include "core/type.h"
 #include "mlu_op_kernel.h"
-#include "kernels/unary_op/unary_op_host.h"
-using namespace std;
+#include "cnrt.h"
+#include "cndev.h"
+
 typedef uint16_t half;
 
+static inline bool isSupportType(const mluOpDataType_t check_type,
+                                 const mluOpDataType_t support_type[],
+                                 const int len) {
+  for (int i = 0; i < len; ++i) {
+    if (check_type == support_type[i]) {
+      return true;
+    }
+  }
+  return false;
+}
 
-mluOpStatus_t MLUOP_WIN_API mluOpMlNms(mluOpHandle_t handle,
-		const mluOpTensorDescriptor_t boxes_data_ptr_desc, void* boxes_data_ptr, void* scores_max_boxes_data_ptr,
-		int input_boxes_num, float iou_threshold, uint8_t* output_boxes_index) {
-	
-	int setoff;
-	bool zero_element = false;
-	mluOpDataType_t data_type = MLUOP_DTYPE_HALF;
-	mluOpDataType_t support_type[2] = {MLUOP_DTYPE_HALF, MLUOP_DTYPE_FLOAT};
+mluOpStatus_t unaryOpNmsParamCheck(
+    const std::string &op_name, const mluOpHandle_t &handle,
+    const mluOpTensorDescriptor_t &x_desc, const void *x,
+    const mluOpDataType_t support_type[], const int &len, bool &zero_element) {
+  PARAM_CHECK(op_name, x_desc != NULL);
+  PARAM_CHECK(op_name, handle != NULL);
 
-	cnrtDim3_t k_dim = {4, 1, 1};
-	cnrtFunctionType_t k_type = CNRT_FUNC_TYPE_UNION1;
-
-	mluOpStatus_t param_check = unaryOpNmsParamCheck(
-			"[mluOpMlNms]", boxes_data_ptr_desc, boxes_data_ptr, scores_max_boxes_data_ptr, support_type, 2, zero_element);
-	if(param_check != MLUOP_STATUS_SUCCESS){
-		return param_check;
-	}
-
-	void (*mluOpFuncKernel)(cnrtDim3_t k_dim, cnrtFunctionType_t k_type, cnrtQueue_t queue, mluOpDataType_t data_type,
-			void* boxes_data_ptr, void* scores_max_boxes_data_ptr, float nmsThres, int input_boxes_num, uint8_t* output_boxes_index);
-	//mluOpFuncKernel = NULL;
-	if (k_type == CNRT_FUNC_TYPE_BLOCK){
-		if (boxes_data_ptr_desc->dtype == MLUOP_DTYPE_HALF){
-			mluOpFuncKernel = mluBlockKernelMlNmsHalfFast;
-		}else {
-			mluOpFuncKernel = mluBlockKernelMlNmsFloatFast;
-		}
-	}else {
-		if (boxes_data_ptr_desc->dtype == MLUOP_DTYPE_HALF){
-			mluOpFuncKernel = mluUnionKernelMlNmsHalfFast;
-		}else{
-			mluOpFuncKernel = mluUnionKernelMlNmsFloatFast;
-		}
-	}
-
-	KERNEL_CHECK(
-			(mluOpFuncKernel(k_dim, k_type, handle->queue, boxes_data_ptr_desc->dtype, boxes_data_ptr, scores_max_boxes_data_ptr, iou_threshold, input_boxes_num, output_boxes_index)));
-	GEN_CASE_END();
-
-	return MLUOP_STATUS_SUCCESS;
-
+  // check data type
+  if (!isSupportType(x_desc->dtype, support_type, len)) {
+    LOG(ERROR) << op_name << ":x_desc's data type is not supported.";
+    return MLUOP_STATUS_BAD_PARAM;
+  }
+  // check 0 element
+  if (mluOpGetTensorElementNum(x_desc) == 0) {
+    VLOG(5) << op_name << "skip zero element tensor.";
+    zero_element = true;
+    return MLUOP_STATUS_SUCCESS;
+  }
+  PARAM_CHECK(op_name, x != NULL);
+  return MLUOP_STATUS_SUCCESS;
 }
 
 
+static void policyFunc(const mluOpHandle_t &handle,
+                       const mluOpTensorDescriptor_t desc, cnrtDim3_t *k_dim,
+                       cnrtFunctionType_t *k_type) {
+  size_t dim = mluOpGetTensorElementNum(desc);
+  // Union1 policyFunc
+  *k_type = CNRT_FUNC_TYPE_UNION1;
+  k_dim->x = handle->core_num_per_cluster;
+  k_dim->y = mluop::runtime::getClusterLimitCapability(handle);
+  k_dim->z = 1;
+  // if a case is smaller than 2048 , it just need one cluster can work best.
+  size_t small_case_thread = 2048;
+  if (dim <= small_case_thread) k_dim->y = 1;
+}
 
+mluOpStatus_t MLUOP_WIN_API mluOpMlNms(mluOpHandle_t handle,
+    const mluOpTensorDescriptor_t boxes_data_ptr_desc, void* boxes_data_ptr,
+    float iou_threshold, void* output_boxes_index) {
+
+    bool zero_element = false;
+    mluOpDataType_t support_type[2] = {MLUOP_DTYPE_HALF, MLUOP_DTYPE_FLOAT};
+    mluOpStatus_t param_check = unaryOpNmsParamCheck(
+      "[mluOpMlNms]", handle, boxes_data_ptr_desc, boxes_data_ptr,
+      support_type, 2, zero_element);
+
+    if (param_check != MLUOP_STATUS_SUCCESS) {
+      return param_check;
+    }
+
+    cnrtDim3_t k_dim;
+    cnrtFunctionType_t k_type;
+    policyFunc(handle, boxes_data_ptr_desc, &k_dim, &k_type);
+    int input_boxes_num = boxes_data_ptr_desc->total_element_num / 4;
+    void (*mluOpFuncKernel)(cnrtDim3_t k_dim, cnrtFunctionType_t k_type,
+      cnrtQueue_t queue, mluOpDataType_t data_type, void* boxes_data_ptr,
+      float nmsThres, int input_boxes_num, uint8_t* output_boxes_index);
+
+      if (boxes_data_ptr_desc->dtype == MLUOP_DTYPE_HALF) {
+          mluOpFuncKernel = mluOpKernelMlNmsHalfFast;
+      } else {
+          mluOpFuncKernel = mluOpKernelMlNmsFloatFast;
+      }
+
+    KERNEL_CHECK(
+      (mluOpFuncKernel(k_dim, k_type, handle->queue,
+         boxes_data_ptr_desc->dtype, boxes_data_ptr,
+         iou_threshold, input_boxes_num, (uint8_t*)output_boxes_index)));
+    GEN_CASE_END();
+
+    return MLUOP_STATUS_SUCCESS;
+}
