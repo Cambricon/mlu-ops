119079b5b57c097f7dfc5e110ee7228e5020bba1
wenzhengyin <71548662+Jones154@users.noreply.github.com>
2022-10-25 13:47:14 +0800

    Update mlu_op.h

--- bangc-ops/mlu_op.h
+++ bangc-ops/mlu_op.h
@@ -1455,7 +1455,7 @@ mluOpGetTensorAndDataFromTensorSet(mluOpTensorSetDescriptor_t tensorSetDesc,
 mluOpStatus_t MLUOP_WIN_API mluOpMlNms(mluOpHandle_t handle,
                                        const mluOpTensorDescriptor_t boxes_data_ptr_desc,
                                        void* boxes_data_ptr,
-		                                   float iou_threshold,
+		                        float iou_threshold,
                                        void* output_boxes_index);
 
 // Group:Abs
