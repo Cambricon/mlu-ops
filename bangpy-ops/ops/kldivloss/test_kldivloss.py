78d297efea0f99d5168326ba407e4fb7ea6c7c54
Sunshine-KK <59988301+Sunshine-KK@users.noreply.github.com>
2022-10-25 21:23:17 +0800

    Update test_kldivloss.py

--- bangpy-ops/ops/kldivloss/test_kldivloss.py
+++ bangpy-ops/ops/kldivloss/test_kldivloss.py
@@ -18,22 +18,28 @@
 # CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-# pylint: disable=too-many-locals
+# pylint: disable=too-many-locals, line-too-long, too-many-function-args
 """KlDivloss test demo."""
+from test import registerOp, OpTest
 import numpy as np
-import pytest
 import bangpy as bp
 from bangpy.common import load_op_by_type
-from kldivloss import DTYPES, KERNEL_NAME, TARGET_LIST
+from kldivloss import KERNEL_NAME, TARGET_LIST
 
 
 def cal_diff(result, data_out, reduction):
     """compute diff"""
     if reduction == 0:
         result[np.isnan(result)] = 0
-        result[np.isinf(result)] = 0
+        result[np.isinf(result)] = 1
         data_out[np.isnan(data_out)] = 0
-        data_out[np.isinf(data_out)] = 0
+        data_out[np.isinf(data_out)] = 1
+    else:
+        if (np.isnan(result) and np.isnan(data_out)) or (np.isinf(result) and np.isinf(data_out)):
+            print("DIFF1:", str(round(0 * 100, 5)) + "%")
+            print("DIFF2:", str(round(0 * 100, 5)) + "%")
+            return
+
 
     bp.assert_allclose(np.isnan(result), np.isnan(data_out))
     bp.assert_allclose(np.isinf(result), np.isinf(data_out))
@@ -58,147 +64,103 @@ def cal_diff(result, data_out, reduction):
     assert round(diff2 * 100, 5) < 3e-3 * 100
 
 
-@pytest.mark.parametrize(
-    "shape",
-    [
-        (2 ** 10,),
-        (2 ** 12,),
-        (2 ** 20,),
-        (2 ** 26,),
-        (2, 2 ** 20,),
-        (2, 4, 8, 1, 2 ** 12,),
-    ],
-)
-@pytest.mark.parametrize(
-    "dtype", DTYPES,
-)
-@pytest.mark.parametrize(
-    "reduction", [0, 1, 2, 3],
-)
-@pytest.mark.parametrize(
-    "log_target", [0, 1],
-)
-def test_kldivloss(target, shape, dtype, reduction, log_target):
-    """Test kldivloss operator by giving multiple sets of parameters."""
-    if target not in TARGET_LIST:
-        return
+@registerOp("kldivloss")
+class KlDivLossOp(OpTest):
+    """Use proto_test to test cosine_embedding_loss."""
+    def __init__(self, target, dtype, tensor_list, output_tensor, param):
+        self.dtype = dtype
+        super().__init__(target, dtype, tensor_list, output_tensor, param)
+        self.param = param
 
-    # Convert the size of the input data to the new style (batchnum, length)
-    print(
-        "shape is :",
-        shape,
-        "reduction is :",
-        reduction,
-        "log_target is :",
-        log_target,
-        "dtype is :",
-        dtype,
-    )
-    inputdim = len(shape)
-
-    batchnum = shape[0]
-    length = 1
-    if inputdim == 1:
-        batchnum = 1
-        length = shape[0]
-    else:
-        for i in range(1, inputdim):
-            length = length * shape[i]
-    shape = (batchnum, length)
-    totaldata = batchnum * length
-
-    #  By default, input has been logged
-    data_input = np.random.uniform(low=0, high=1, size=shape).astype(
-        dtype.as_numpy_dtype
-    )
-    data_input = np.log(data_input)
-
-    data_target = np.random.uniform(low=0, high=1, size=shape).astype(
-        dtype.as_numpy_dtype
-    )
-
-    # Output argument
-    if log_target == 0:
-        data_out = np.multiply(
-            data_target, np.subtract(np.log(data_target), (data_input))
+    def evaluate(self, f, dtype, target):
+        """Test kldivloss operator by giving multiple sets of parameters."""
+        if target not in TARGET_LIST:
+            return
+        reduction = self.param.get("kldivlossParam").get("reduction")
+        log_target =  self.param.get("kldivlossParam").get("logTarget")
+        shape =   self.inputs_tensor_list[0].shape
+        # Convert the size of the input data to the new style (batchnum, length)
+        print(
+            "shape is :", shape, "reduction is :", reduction, "log_target is :", log_target
         )
-    elif log_target == 1:
-        data_out = np.multiply(
-            np.exp(data_target), np.subtract((data_target), (data_input))
+        data_in0 = self.inputs_tensor_list[0]
+        data_in1 = self.inputs_tensor_list[1]
+        data_out = self.output_tensor_list[0]
+
+        # Reduction operation
+        if reduction == 0:
+            print("data_out : ", data_out)
+        if reduction == 1:
+            print("data_out_sum : ", data_out)
+        if reduction == 2:
+            print("data_out_mean : ", data_out)
+        if reduction == 3:
+            print("data_out_batchmean : ", data_out)
+
+        dev = bp.device(0)
+        # Set I/O data
+        data_input_dev = bp.Array(data_in0.reshape(shape[0], data_in0.size // shape[0]).astype(dtype.as_numpy_dtype), dev)
+        data_target_dev = bp.Array(data_in1.reshape(shape[0], data_in0.size // shape[0]).astype(dtype.as_numpy_dtype), dev)
+        data_out_dev = bp.Array(
+            np.zeros(data_in0.flatten().shape, dtype.as_numpy_dtype), dev
         )
-    # Reduction operation
-    if reduction == 0:
-        print("data_out : ", data_out)
-    if reduction == 1:
-        data_out_sum = np.sum(data_out)
-        print("data_out_sum : ", data_out_sum)
-    if reduction == 2:
-        data_out_mean = np.mean(data_out)
-        print("data_out_mean : ", data_out_mean)
-    if reduction == 3:
-        data_out_batchmean = np.sum(data_out) / batchnum
-        print("data_out_batchmean : ", data_out_batchmean)
 
-    dev = bp.device(0)
-    # Set I/O data
-    data_input_dev = bp.Array(data_input.astype(dtype.as_numpy_dtype), dev)
-    data_target_dev = bp.Array(data_target.astype(dtype.as_numpy_dtype), dev)
-    data_out_dev = bp.Array(
-        np.zeros(data_out.flatten().shape, dtype.as_numpy_dtype), dev
-    )
-    reduction_result_dev = bp.Array(np.zeros((1,), np.float32), dev)
-
-    f = load_op_by_type(KERNEL_NAME, dtype.name)
-    f(
-        data_input_dev,
-        data_target_dev,
-        data_out_dev,
-        batchnum,
-        length,
-        reduction,
-        log_target,
-        reduction_result_dev,
-    )
-
-    evaluator = f.time_evaluator(number=100, repeat=2, min_repeat_ms=0)
-    t = (
-        evaluator(
+        f = load_op_by_type(KERNEL_NAME, dtype.name)
+        f(
             data_input_dev,
             data_target_dev,
             data_out_dev,
-            batchnum,
-            length,
+            shape[0],
+            data_in0.size // shape[0],
             reduction,
             log_target,
-            reduction_result_dev,
-        ).mean
-        * 1e3
-    )
+        )
 
-    data_out_dev = data_out_dev.numpy().reshape(shape)
-    reduction_result_dev = reduction_result_dev.numpy()
-    if reduction == 0:
-        print("data_out_dev : ", data_out_dev)
-        cal_diff(data_out, data_out_dev, reduction)
-    elif reduction == 1:
-        print("data_out_sum_dev : ", reduction_result_dev[0])
-        cal_diff(data_out_sum, reduction_result_dev[0], reduction)
-    elif reduction == 2:
-        print("data_out_mean_dev : ", reduction_result_dev[0])
-        cal_diff(data_out_mean, reduction_result_dev[0], reduction)
-    elif reduction == 3:
-        print("data_out_batchmean_dev : ", reduction_result_dev[0])
-        cal_diff(data_out_batchmean, reduction_result_dev[0], reduction)
+        evaluator = f.time_evaluator(number=100, repeat=2, min_repeat_ms=0)
+        t = (
+            evaluator(
+                data_input_dev,
+                data_target_dev,
+                data_out_dev,
+                shape[0],
+                data_in0.size // shape[0],
+                reduction,
+                log_target,
+            ).mean
+            * 1e3
+        )
 
-    print("tutorial : %f ms" % t)
+        data_out_dev = data_out_dev.numpy().reshape(shape[0], data_in0.size // shape[0])
+        if reduction == 0:
+            print("data_out_dev : ", data_out_dev)
+            data_out = data_out.reshape(shape[0], data_in0.size // shape[0])
+            cal_diff(data_out, data_out_dev, reduction)
+        elif reduction == 1:
+            print("data_out_sum_dev : ", data_out_dev[0])
+            cal_diff(data_out[0], data_out_dev[0][0], reduction)
+        elif reduction == 2:
+            print("data_out_mean_dev : ", data_out_dev[0])
+            cal_diff(data_out[0], data_out_dev[0][0], reduction)
+        elif reduction == 3:
+            print("data_out_batchmean_dev : ", data_out_dev[0])
+            cal_diff(data_out[0], data_out_dev[0][0], reduction)
 
-    # io_efficiency
-    theory_io_size = (
-        3 * totaldata * dtype.bytes if reduction == 0 else 2 * totaldata * dtype.bytes
-    )
-    # io_bandwidth = 2 ** 40  # MLU290: 1024GB/s
-    io_bandwidth = 307.2 * 2 ** 30  # MLU370-s4: 307.2GB/s
-    io_efficiency = 1000 * theory_io_size / (t * io_bandwidth)
-    print("theory_io_size : %f GB" % (theory_io_size / (2 ** 30)))
-    print("io_efficiency:", str(round(io_efficiency * 100, 2)) + "%")
-    print("------------------------------------------------------------")
+        print("tutorial : %f ms" % t)
+
+        # io_efficiency
+        theory_io_size = (
+            3 * data_in0.size * dtype.bytes if reduction == 0 else 2 * data_in0.size * dtype.bytes
+        )
+        # io_bandwidth = 2 ** 40  # MLU290: 1024GB/s
+        io_bandwidth = 307.2 * 2 ** 30  # MLU370-s4: 307.2GB/s
+        io_efficiency = 1000 * theory_io_size / (t * io_bandwidth)
+        print("theory_io_size : %f GB" % (theory_io_size / (2 ** 30)))
+        print("io_efficiency:", str(round(io_efficiency * 100, 2)) + "%")
+        print("------------------------------------------------------------")
+
+    def compute(self):
+        if self.target not in TARGET_LIST:
+            return
+        f = load_op_by_type(KERNEL_NAME, self.dtype.name)
+
+        self.evaluate(f, self.dtype, self.target)
