9d2ea26fd0580104d304e019cbf1903a70345ed8
gala <121872426@qq.com>
2022-11-16 18:34:43 +0800

    修改测试用例生成函数

--- bangpy-ops/ops/logaddexp/test_logaddexp.py
+++ bangpy-ops/ops/logaddexp/test_logaddexp.py
@@ -70,10 +70,8 @@ def run(input_x, input_y, dtype):
 
 
 def random_int_list(max_dim_length, each_dim_max_length):
-    random_list = []
-    for _ in range(max_dim_length):
-        random_list.append(random.randint(2, each_dim_max_length))
-    return tuple(random_list)
+    return (random.randint(2, each_dim_max_length)
+        for _ in range(max_dim_length))
 
 
 def create_shape_list(
