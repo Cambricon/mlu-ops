f54a8b678df301dae609e340ce3305bf4f305dc7
wenzhengyin <71548662+Jones154@users.noreply.github.com>
2022-12-13 15:51:56 +0800

    Update ml_nms.cpp

--- bangc-ops/kernels/ml_nms/ml_nms.cpp
+++ bangc-ops/kernels/ml_nms/ml_nms.cpp
@@ -31,6 +31,7 @@
 #include "mlu_op_kernel.h"
 #include "cnrt.h"
 #include "cndev.h"
+#include "ml_nms.h"
 
 static inline bool isSupportType(const mluOpDataType_t check_type,
                                  const mluOpDataType_t support_type[],
