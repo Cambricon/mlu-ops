ff03e1cf467ea60be7df1ace89ad909419753676
XuMingy <44832949+AIYoungcino@users.noreply.github.com>
2022-12-29 14:50:10 +0800

    Update execuator.py

--- bangpy-ops/test/execuator.py
+++ bangpy-ops/test/execuator.py
@@ -58,5 +58,6 @@ def execuate_kernel(proto_file, target: str, op_name):
     else:
         raise TypeError("Unsupported data type %s" % (data.get_output_dtype()))
     # launch kernel in factory model
+    print("aaa",target, dtype, inputs, output, data.read_prototxt())
     op_test = OpTestFactory.factory(op_name)(target, dtype, inputs, output, data.read_prototxt())
     op_test.compute()
