c7898bcbb449a773974b498d0ebe1f030019720b
wangyuan <wangyuan@cambricon.com>
2023-03-07 17:09:28 +0800

    [Feature](bangc-ops): sup mlu570,580,585

--- bangc-ops/core/context.cpp
+++ bangc-ops/core/context.cpp
@@ -41,7 +41,11 @@ struct deviceName name_list_table[] = {
     {"MLU220", MLUOP_MLU220},
     {"MLU220 SOC", MLUOP_MLU220},
     {"MLU290", MLUOP_MLU290},
+    {"MLU365-D2", MLUOP_MLU370},
     {"MLU370", MLUOP_MLU370},
+    {"MLU570", MLUOP_MLU590},
+    {"MLU580", MLUOP_MLU590},
+    {"MLU585", MLUOP_MLU590},
     {"MLU590", MLUOP_MLU590}
     // mluOp not support mlu100 only for error case.
     // {"MLU100", MLUOP_MLU100},
