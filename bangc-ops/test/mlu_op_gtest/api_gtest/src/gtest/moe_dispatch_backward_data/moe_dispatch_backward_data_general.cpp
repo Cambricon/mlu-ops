ece3dfc5fe44767e0940efed9bba79ec7df50b6a
wangxin1 <wangxin1@cambricon.com>
2023-03-17 17:39:59 +0800

    [TEST](bangc-ops):add moe_dispatch_backward_data api check cases

--- bangc-ops/test/mlu_op_gtest/api_gtest/src/gtest/moe_dispatch_backward_data/moe_dispatch_backward_data_general.cpp
+++ bangc-ops/test/mlu_op_gtest/api_gtest/src/gtest/moe_dispatch_backward_data/moe_dispatch_backward_data_general.cpp
@@ -147,7 +147,6 @@ class moe_dispatch_backward_data_general
                        mluOpDataTypeBytes(grad_input_params.get_dtype()) *
                            mluOpGetTensorElementNum(grad_input_desc_)));
       }
-
     } catch (const std::exception &e) {
       FAIL() << "MLUOPAPIGTEST: catched " << e.what()
              << " in moe_dispatch_backward_data_general.";
