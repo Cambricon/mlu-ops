85ab1941c1e75e45483e7a8f706cfdd0cdd55556
guohaodong <guohaodong@cambricon.com>
2023-05-12 16:49:34 +0800

    [Docs](bangc-ops): update the License for nms and roialign_forward.

--- bangc-ops/test/mlu_op_gtest/pb_gtest/src/zoo/roialign_forward/roialign_forward.cpp
+++ bangc-ops/test/mlu_op_gtest/pb_gtest/src/zoo/roialign_forward/roialign_forward.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright (C) [2019-2022] by Cambricon, Inc.
+ * Copyright (C) [2022] by Cambricon, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
@@ -7,8 +7,6 @@
  * without limitation the rights to use, copy, modify, merge, publish,
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
-set title
-set title
  * the following conditions:
  *
  * The above copyright notice and this permission notice shall be included
