ebd146b7fcacccae06f3b9a3eb523f0226977a44
defei-coder <1755086696@qq.com>
2023-06-08 10:11:53 +0800

    [Feature](bangc-ops):add file name format check

---
+++ tools/check_file_name.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# Copyright (C) [2023] by Cambricon, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall self.tcp included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS self.tcp LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import os
+import sys
+
+def file_name(dir):
+  for root, dirs, _ in os.walk(dir):
+    for dir in dirs:
+      file_name = []
+      for  _, _, files in os.walk(os.path.join(root, dir)):
+        for file in files:
+          if file.endswith(".mlu") or file.endswith(".cpp"):
+            name = file.split('.')[0]
+            if name in file_name:
+              print('-- [check_file_name error] find %s.cpp and %s.mlu in directory %s,'\
+                     %(name, name, os.path.join(root, dir)), 'the same name is forbidden.' )
+              exit(-1)
+            file_name.append(name)
+
+
+def main():
+    dir_path = sys.argv[1]
+    file_name(dir_path)
+
+
+if __name__ == "__main__":
+    main()
