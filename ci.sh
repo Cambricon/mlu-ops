686149d370ebfed9980d3836716715c3d7da5599
dongchengwei <dongchengwei@cambricon.com>
2023-07-05 10:15:45 +0800

    update ci.sh.

--- ci.sh
+++ ci.sh
@@ -1,5 +1,6 @@
 # /bin/bash
 # get PR id
+set -e
 PR_string=$(echo $GITHUB_REF | grep -Eo "/[0-9]*/")
 pr_id=(${PR_string//// })
 
@@ -15,7 +16,7 @@ card_type="MLU370-S4"
 repo_name="mlu-ops-dev"
 
 # repo ci root path
-repo_root="/github_ci/${repo_name}_ci/"
+repo_root="/home/cambricon/${repo_name}_ci/"
 if [ ! -d $repo_root ];then
     mkdir $repo_root
 fi
@@ -65,8 +66,8 @@ chmod o+w "$request_root/log_list"
 echo "${repo_name},${pr_id},${currentTimeStamp},${card_type}" > "$requests_path/${request_name}"
 
 # change dir group for server and client, or when server/client try to delete request, ftp may raise error.
-chgrp -R ftpuser $request_root
-chgrp -R ftpuser $requests_path
+chgrp -R cambricon $request_root
+chgrp -R cambricon $requests_path
 
 # start script
 python3 file_guard.py "$request_root/status" "$request_root/log" &
@@ -77,6 +78,8 @@ wait
 # status=$(cat ${request_root}/status)
 
 status=$( head -n +1 ${request_root}/status )
+
+set +e
 
 if [ "$status" != "success" ];then
     return_info=$( sed -n 2p ${request_root}/status )
