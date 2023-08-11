ded9788ddcca3e3ef6cff8958ea55adefc50827e
liuduanhui <103939338+DanieeelLiu@users.noreply.github.com>
2023-08-11 15:28:43 +0800

    [Fix](bangc-ops): revise sample compile shell script for local compiling user friendly (#794)

--- samples/bangc-ops/env.sh
+++ samples/bangc-ops/env.sh
@@ -1,4 +1,4 @@
-SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_PATH="$(cd "../../$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export BANGC_HOME=${SCRIPT_PATH}/bangc-ops
 
 if [[ -z ${NEUWARE_HOME} ]]; then
@@ -8,7 +8,19 @@ else
   echo "env 'NEUWRAE_HOME': ${NEUWARE_HOME}."
 fi
 
+MLU_OP_H=${NEUWARE_HOME}/include/mlu_op.h
+
+if [[ ! -f ${MLU_OP_H} || -h ${MLU_OP_H} ]]; then
+  ln -sf ${BANGC_HOME}/mlu_op.h ${MLU_OP_H}
+  for MLU_OPS_SO in $(ls ${BANGC_HOME}/build/lib/)
+  do 
+    if [[ ${MLU_OPS_SO} == libmluops.so* ]]; then
+      ln -sf ${BANGC_HOME}/build/lib/${MLU_OPS_SO} ${NEUWARE_HOME}/lib64/${MLU_OPS_SO}
+    fi
+  done
+fi
+
 export PATH=${NEUWARE_HOME}/bin:${PATH}
-export LD_LIBRARY_PATH=${NEUWARE_HOME}/lib64:${BANGC_HOME}/build/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${NEUWARE_HOME}/lib64:$LD_LIBRARY_PATH
 
 echo "Ready for mlu-ops-building."
