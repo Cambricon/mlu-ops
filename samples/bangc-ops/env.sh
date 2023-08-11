cc0314d1ba3cf28edabcd71565acea67304a52ed
DanieeelLiu <liuduanhui@cambricon.com>
2023-08-11 15:15:57 +0800

    [Fix](bangc-ops): optimze shell code for sample env set.

--- samples/bangc-ops/env.sh
+++ samples/bangc-ops/env.sh
@@ -9,15 +9,15 @@ else
 fi
 
 MLU_OP_H=${NEUWARE_HOME}/include/mlu_op.h
-MLU_OPS_SO=${NEUWARE_HOME}/lib64/libmluops.so
 
 if [[ ! -f ${MLU_OP_H} || -h ${MLU_OP_H} ]]; then
   ln -sf ${BANGC_HOME}/mlu_op.h ${MLU_OP_H}
-fi
-
-if [[ ! -f ${MLU_OPS_SO} || -h ${MLU_OPS_SO} ]]; then
-  ln -sf ${BANGC_HOME}/build/lib/libmluops.so ${MLU_OPS_SO}
-  ln -sf ${BANGC_HOME}/build/lib/libmluops.so.0 ${NEUWARE_HOME}/lib64/libmluops.so.0
+  for MLU_OPS_SO in $(ls ${BANGC_HOME}/build/lib/)
+  do 
+    if [[ ${MLU_OPS_SO} == libmluops.so* ]]; then
+      ln -sf ${BANGC_HOME}/build/lib/${MLU_OPS_SO} ${NEUWARE_HOME}/lib64/${MLU_OPS_SO}
+    fi
+  done
 fi
 
 export PATH=${NEUWARE_HOME}/bin:${PATH}
