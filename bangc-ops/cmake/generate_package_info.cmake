980999551d77bd6dbdf5c46c8f5271be2ac99cc5
DanieeelLiu <liuduanhui@cambricon.com>
2023-11-22 11:57:49 +0800

    [Feature](bangc-ops): do package info set independent when do packaging.

---
+++ bangc-ops/cmake/generate_package_info.cmake
@@ -0,0 +1,64 @@
+################################################################################
+# Generate package info
+################################################################################
+message("generating package configuration")
+set(PKG_NAME "mluops")
+set(PKG_VERSION "${BUILD_VERSION}")
+set(PKG_RELEASE "1")
+set(PKG_PROVIDES "mlu-ops")
+execute_process(COMMAND sh -c "LANG=en date '+%a, %d %b %G %X %z'" OUTPUT_VARIABLE PKG_DATE)
+execute_process(COMMAND sh -c "LANG=en date '+%a %b %d %Y'" OUTPUT_VARIABLE PKG_DATE2 OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+execute_process(
+  COMMAND grep "^ID=" "/etc/os-release"
+  COMMAND cut -d = -f2
+  COMMAND tr '[[:upper:]]' '[[:lower:]]'
+  COMMAND xargs
+  OUTPUT_VARIABLE HOST_DISTRO
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+execute_process(
+  COMMAND grep "^VERSION_ID=" "/etc/os-release"
+  COMMAND cut -d = -f2
+  COMMAND xargs
+  OUTPUT_VARIABLE HOST_DISTRO_VERSION
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+message("HOST_DISTRO: ${HOST_DISTRO}")
+if ( "${HOST_DISTRO}" MATCHES "ubuntu|debian|linuxmint" )
+  # we could also check ID_LIKE and /etc/debian_version
+  set(HINT_DEB_DISTRO "${HOST_DISTRO}${HOST_DISTRO_VERSION}")
+else()
+  set(HINT_DEB_DISTRO "")
+endif()
+if( "${HOST_DISTRO}" MATCHES "centos|scientific" )
+  # have /etc/redhat-release
+  set(HINT_RPM_DISTRO "el${HOST_DISTRO_VERSION}")
+elseif("${HOST_DISTRO}" MATCHES "fedora")
+  set(HINT_RPM_DISTRO "f${HOST_DISTRO_VERSION}")
+else()
+  set(HINT_RPM_DISTRO "")
+endif()
+set(DEB_DISTRO "${HINT_DEB_DISTRO}")
+set(RPM_DISTRO "${HINT_RPM_DISTRO}")
+# get codename of Linux distribution, we could read VERSION_CODENAME in /etc/os-release
+# but at present, I just save these codenames in cmake itself
+set(MAP_ubuntu16.04 "xenial")
+set(MAP_ubuntu18.04 "bionic")
+set(MAP_ubuntu20.04 "focal")
+set(MAP_ubuntu20.10 "groovy")
+set(MAP_ubuntu21.04 "hirsute")
+set(MAP_debian9 "stretch")
+set(MAP_debian10 "buster")
+set(MAP_debian11 "bullseye")
+set(PKG_DISTRIBUTION "${MAP_${DEB_DISTRO}}")
+configure_file(
+  "${CMAKE_SOURCE_DIR}/../installer/centos7.5/SPECS/mluops-independent.spec.in"
+  "${CMAKE_SOURCE_DIR}/../installer/centos7.5/SPECS/mluops-independent.spec"
+  @ONLY
+)
+configure_file(
+  "${CMAKE_SOURCE_DIR}/../installer/independent/debian/changelog.in"
+  "${CMAKE_SOURCE_DIR}/../installer/independent/debian/changelog"
+  @ONLY
+)
