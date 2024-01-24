1f04352227582fed0b03d7e285f36c28c371c386
mahxn0 <1262384588@qq.com>
2024-01-24 18:57:28 +0800

    [Feature](mlu-ops):modify cmake and kernels content.

---
+++ kernels/border_align/border_align_backward/border_align_backward.h
@@ -0,0 +1,34 @@
+/*************************************************************************
+ * Copyright (C) [2023] by Cambricon, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *************************************************************************/
+#ifndef KERNELS_BORDER_ALIGN_BACKWARD_BORDER_ALIGN_BACKWARD_H_
+#define KERNELS_BORDER_ALIGN_BACKWARD_BORDER_ALIGN_BACKWARD_H_
+
+#include "mlu_op.h"
+
+mluOpStatus_t MLUOP_WIN_API KernelBorderAlignBackward(
+    cnrtDim3_t k_dim, cnrtFunctionType_t k_type, cnrtQueue_t queue,
+    mluOpDataType_t d_type, const void *grad_output, const void *boxes,
+    const int32_t *argmax_idx, const int32_t pool_size, const int32_t origin_n,
+    const int32_t origin_h, const int32_t origin_w, const int32_t origin_c,
+    const int32_t origin_k, void *grad_input);
+#endif  // KERNELS_BORDER_ALIGN_BACKWARD_BORDER_ALIGN_BACKWARD_H_
