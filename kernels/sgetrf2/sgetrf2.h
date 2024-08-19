e678393d51ff3a3d7b4a043a735509f719329528
chuancysuen <379087671@qq.com>
2024-08-20 03:51:39 +0800

    [Fix](mluOpSgetrf2): fix some bugs, reset workspace and update docs

--- kernels/sgetrf2/sgetrf2.h
+++ kernels/sgetrf2/sgetrf2.h
@@ -30,11 +30,11 @@
 #define MAX_DIM 65532
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
-#define dA(i_, j_) (dA + (i_) * ldda + (j_))
-#define d_rA(i_, j_) (d_rA + (i_) * ldda + (j_))
-#define d_iA(i_, j_) (d_iA + (i_) * ldda + (j_))
-#define dAT(i_, j_) (dAT + (i_) + (j_) * lddat)
-#define dAP(i_, j_) (dAP + (i_) * nb + (j_))
+#define dA(i_, j_) (dA + (i_)*ldda + (j_))
+#define d_rA(i_, j_) (d_rA + (i_)*ldda + (j_))
+#define d_iA(i_, j_) (d_iA + (i_)*ldda + (j_))
+#define dAT(i_, j_) (dAT + (i_) + (j_)*lddat)
+#define dAP(i_, j_) (dAP + (i_)*nb + (j_))
 #define c_one 1.0
 #define c_neg_one -1.0
 #define taskType 8
@@ -44,127 +44,88 @@
 #define CEILDIV(x, y) ((x + y - 1) / y)
 #define ROUNDUP(x, y) (CEILDIV(x, y) * y)
 
+mluOpStatus_t MLUOP_WIN_API KernelScal_ger(
+    cnrtDim3_t k_dim, cnrtFunctionType_t k_type, cnrtQueue_t queue,
+    mluOpDataType_t d_type, int batch, int M_size, int N_size, int ib, int J,
+    int m, int n, int step, float *dA, int lda, int stride_a, float *workspace,
+    int *dipiv, int *dipiv2, int *info, int gbstep, int mode);
+mluOpStatus_t MLUOP_WIN_API KernelCcal_ger(
+    cnrtDim3_t k_dim, cnrtFunctionType_t k_type, cnrtQueue_t queue,
+    mluOpDataType_t d_type, int batch, int M_size, int N_size, int ib, int J,
+    int m, int n, int step, float *d_rA, float *d_iA, int lda, int stride_a,
+    float *workspace, int *dipiv, int *dipiv2, int *info, int gbstep, int mode);
 
-mluOpStatus_t MLUOP_WIN_API KernelScal_ger(cnrtDim3_t k_dim, cnrtFunctionType_t k_type,
-                                           cnrtQueue_t queue, mluOpDataType_t d_type,
-                                           int batch, int M_size, int N_size, int ib, int J,
-                                           int m, int n, int step,
-                                           float *dA, int lda, int stride_a, float *workspace,
-                                           int *dipiv, int *dipiv2, int *info, int gbstep, int mode);
-mluOpStatus_t MLUOP_WIN_API KernelCcal_ger(cnrtDim3_t k_dim, cnrtFunctionType_t k_type,
-                                           cnrtQueue_t queue, mluOpDataType_t d_type,
-                                           int batch, int M_size, int N_size, int ib, int J,
-                                           int m, int n, int step,
-                                           float *d_rA, float *d_iA, int lda, int stride_a, float *workspace,
-                                           int *dipiv, int *dipiv2, int *info, int gbstep, int mode);
-
-mluOpStatus_t MLUOP_WIN_API KernelMatrixAdd(
-    cnrtDim3_t k_dim, cnrtFunctionType_t k_type,
-    cnrtQueue_t queue, mluOpDataType_t d_type,
-    int batch, int m, int n,
-    float *dA, int ldda, int stride_a,
-    float *dB, int lddb, int stride_b,
-    float *dC, int lddc, int stride_c);
+mluOpStatus_t MLUOP_WIN_API
+KernelMatrixAdd(cnrtDim3_t k_dim, cnrtFunctionType_t k_type, cnrtQueue_t queue,
+                mluOpDataType_t d_type, int batch, int m, int n, float *dA,
+                int ldda, int stride_a, float *dB, int lddb, int stride_b,
+                float *dC, int lddc, int stride_c);
 
 mluOpStatus_t MLUOP_WIN_API KernelMyCnrtMemcpy2D(
-    cnrtDim3_t k_dim, cnrtFunctionType_t k_type,
-    cnrtQueue_t queue, mluOpDataType_t d_type,
-    int batch, int m, int n,
-    float *dA, int ldda, int stride_a,
-    float *dB, int lddb, int stride_b,
-    int mode);
+    cnrtDim3_t k_dim, cnrtFunctionType_t k_type, cnrtQueue_t queue,
+    mluOpDataType_t d_type, int batch, int m, int n, float *dA, int ldda,
+    int stride_a, float *dB, int lddb, int stride_b, int mode);
 
 mluOpStatus_t MLUOP_WIN_API KernelInverse(
-    cnrtDim3_t k_dim, cnrtFunctionType_t k_type,
-    cnrtQueue_t queue, mluOpDataType_t dtype,
-    int batch,
-    float *d_input, int ld_input, int stride_input,
-    float *d_output, int ld_output, int stride_output,
-    int m);
+    cnrtDim3_t k_dim, cnrtFunctionType_t k_type, cnrtQueue_t queue,
+    mluOpDataType_t dtype, int batch, float *d_input, int ld_input,
+    int stride_input, float *d_output, int ld_output, int stride_output, int m);
 
 mluOpStatus_t MLUOP_WIN_API KernelComplexInverse(
-    cnrtDim3_t k_dim, cnrtFunctionType_t k_type,
-    cnrtQueue_t queue, mluOpDataType_t dtype,
-    int batch,
-    float *rd_input, float *id_input, int ld_input, int stride_input,
-    float *rd_output, float *id_output, int ld_output, int stride_output,
-    int m);
+    cnrtDim3_t k_dim, cnrtFunctionType_t k_type, cnrtQueue_t queue,
+    mluOpDataType_t dtype, int batch, float *rd_input, float *id_input,
+    int ld_input, int stride_input, float *rd_output, float *id_output,
+    int ld_output, int stride_output, int m);
 
-mluOpStatus_t MLUOP_WIN_API KernelSwap(cnrtDim3_t k_dim, cnrtFunctionType_t k_type,
-                                       cnrtQueue_t queue, mluOpDataType_t d_type,
-                                       int batch, int M_size, int N_size, int ib, int J,
-                                       int m, int n, int step,
-                                       float *dA, float *d_rA, float *d_iA, int lda, int stride_a,
-                                       int *dipiv, int *dipiv2, int *info, int gbstep);
+mluOpStatus_t MLUOP_WIN_API KernelSwap(
+    cnrtDim3_t k_dim, cnrtFunctionType_t k_type, cnrtQueue_t queue,
+    mluOpDataType_t d_type, int batch, int M_size, int N_size, int ib, int J,
+    int m, int n, int step, float *dA, float *d_rA, float *d_iA, int lda,
+    int stride_a, int *dipiv, int *dipiv2, int *info, int gbstep);
 
-mluOpStatus_t cgemm(mluOpHandle_t handle, mluOpDataType_t dtype,
-                    int m_, int n_, int k_,
-                    int batch, int m, int n,
-                    float *d_ra, float *d_ia, int lda,
-                    float *d_rb, float *d_ib, int ldb,
-                    float *d_rc, float *d_ic, int ldc,
-                    int ldda,
-                    cnrtQueue_t queue);
+mluOpStatus_t cgemm(mluOpHandle_t handle, mluOpDataType_t dtype, int m_, int n_,
+                    int k_, int batch, int m, int n, float *d_ra, float *d_ia,
+                    int lda, float *d_rb, float *d_ib, int ldb, float *d_rc,
+                    float *d_ic, int ldc, int ldda, cnrtQueue_t queue);
 mluOpStatus_t complex_inverse(mluOpHandle_t handle, mluOpDataType_t dtype,
-                              int batch,
-                              float *rd_input, float *id_input, int ld_input, int stride_input,
-                              float *rd_output, float *id_output, int ld_output, int stride_output,
-                              int m,
-                              cnrtQueue_t queue);
-mluOpStatus_t ctrsm(mluOpHandle_t handle, mluOpDataType_t dtype,
-                    int batch, int M, int N,
-                    int m, int n,
-                    float *d_ra, float *d_ia, int lda,
-                    float *d_rb, float *d_ib, int ldb,
+                              int batch, float *rd_input, float *id_input,
+                              int ld_input, int stride_input, float *rd_output,
+                              float *id_output, int ld_output,
+                              int stride_output, int m, cnrtQueue_t queue);
+mluOpStatus_t ctrsm(mluOpHandle_t handle, mluOpDataType_t dtype, int batch,
+                    int M, int N, int m, int n, float *d_ra, float *d_ia,
+                    int lda, float *d_rb, float *d_ib, int ldb,
                     float *work_space, cnrtQueue_t queue);
 
-mluOpStatus_t MLUOP_WIN_API gemm3(
-    mluOpHandle_t handle, mluOpDataType_t dtype,
-    int m_, int n_, int k_,
-    float alpha, float beta,
-    int batch, int m, int n,
-    float *dev_a, float *dev_b, float *dev_c, float *dev_d,
-    int ldda,
-    cnrtQueue_t queue);
-mluOpStatus_t MLUOP_WIN_API gemm4(
-    mluOpHandle_t handle, mluOpDataType_t dtype,
-    int m_, int n_, int k_,
-    float alpha, float beta,
-    int batch, int m, int n,
-    float *dev_a, float *dev_b, float *dev_c, float *dev_d,
-    int ldda,
-    cnrtQueue_t queue);
+mluOpStatus_t MLUOP_WIN_API gemm(mluOpHandle_t handle, mluOpDataType_t dtype,
+                                 int m_, int n_, int k_, float alpha,
+                                 float beta, int batch, int m, int n,
+                                 float *dev_a, float *dev_b, float *dev_c,
+                                 float *dev_d, int ldda, cnrtQueue_t queue);
+mluOpStatus_t MLUOP_WIN_API gemm_for_ctrsm(
+    mluOpHandle_t handle, mluOpDataType_t dtype, int m_, int n_, int k_,
+    float alpha, float beta, int batch, int m, int n, float *dev_a,
+    float *dev_b, float *dev_c, float *dev_d, int ldda, cnrtQueue_t queue);
 
-mluOpStatus_t MLUOP_WIN_API transpose(
-    mluOpHandle_t handle, mluOpDataType_t dtype,
-    int batch, int m, int n,
-    float *input, float *workspace_dst,
-    cnrtQueue_t queue);
-mluOpStatus_t MLUOP_WIN_API transpose_back(
-    mluOpHandle_t handle, mluOpDataType_t dtype,
-    int batch, int m, int n,
-    float *output, void *workspace,
-    cnrtQueue_t queue);
-mluOpStatus_t MLUOP_WIN_API MyCnrtMemcpy2D(
-    mluOpHandle_t handle,
-    int batch, int m, int n,
-    float *dA, int ldda, int stride_a,
-    float *dB, int lddb, int stride_b,
-    int mode,
-    cnrtQueue_t queue);
+mluOpStatus_t MLUOP_WIN_API transpose(mluOpHandle_t handle,
+                                      mluOpDataType_t dtype, int batch, int m,
+                                      int n, float *input, float *workspace_dst,
+                                      cnrtQueue_t queue);
+mluOpStatus_t MLUOP_WIN_API transpose_back(mluOpHandle_t handle,
+                                           mluOpDataType_t dtype, int batch,
+                                           int m, int n, float *output,
+                                           void *workspace, cnrtQueue_t queue);
+mluOpStatus_t MLUOP_WIN_API MyCnrtMemcpy2D(mluOpHandle_t handle, int batch,
+                                           int m, int n, float *dA, int ldda,
+                                           int stride_a, float *dB, int lddb,
+                                           int stride_b, int mode,
+                                           cnrtQueue_t queue);
 
-int sgetrf2_native(
-    mluOpHandle_t handle,
-    mluOpDataType_t dtype,
-    int batch, int m, int n,
-    float *dA, float *d_rA, float *d_iA, int ldda, int gbm, int gbn,
-    int *dipiv, int *dinfo,
-    int gbstep, int mode, void *workspace, cnrtQueue_t queue);
+int sgetrf2_native(mluOpHandle_t handle, mluOpDataType_t dtype, int batch,
+                   int m, int n, float *dA, float *d_rA, float *d_iA, int ldda,
+                   int gbm, int gbn, int *dipiv, int *dinfo, int gbstep,
+                   int mode, void *workspace, cnrtQueue_t queue);
 
-int sgetrf_mlu(
-    mluOpHandle_t handle,
-    mluOpDataType_t dtype,
-    int batch, int m, int n,
-    float *dA, float *d_rA, float *d_iA, int ldda,
-    int *ipiv,
-    int *info, int mode, void *workspace);
+int sgetrf_mlu(mluOpHandle_t handle, mluOpDataType_t dtype, int batch, int m,
+               int n, float *dA, float *d_rA, float *d_iA, int ldda, int *ipiv,
+               int *info, int mode, void *workspace);
--- mlu_op.h
+++ mlu_op.h
@@ -14366,27 +14366,36 @@ mluOpExecFFT(mluOpHandle_t handle,
 mluOpStatus_t MLUOP_WIN_API
 mluOpDestroyFFTPlan(mluOpFFTPlan_t fft_plan);
 /*!
- * @brief Computes the LU decomposition of a matrix using the input tensor descriptor \p input_desc and writes the result to the output tensor descriptor \p output_desc.
+ * @brief Computes the LU decomposition of a matrix using the input tensor descriptor \p input_desc and writes the
+ result to the output tensor descriptor \p output_desc.
+
  * @param[in] handle
  *   Handle to a Cambricon MLUOP context that is used to manage MLU devices and
  *   queues in the deformable convolution backward data operation. For detailed information,
  *   see ::mluOpHandle_t.
+ *
  * @param[in] x_desc
  *   The descriptor of the input tensor. For detailed information,
  *   see ::mluOpTensorDescriptor_t.
+ *
  * @param[in] x
  *   Pointer to the MLU memory that stores the input tensor.
+ *
  * @param[in] y_desc
  *   The descriptor of the output tensor. For detailed information,
  *   see ::mluOpTensorDescriptor_t.
+ *
  * @param[out] y
  *   Pointer to the MLU memory that stores the output tensor.
+ *
  * @param[in, out] workspace
  * Pointer to the MLU memory that is used as an extra workspace for the
  * ::mluOpSgetrf2.
+ *
  * @param[in, out] ipiv
  * INTEGER array, dimension (m);
  * The pivot indices; row i of the matrix was interchanged with row IPIV(i)
+ *
  * @param[out] info
  *     -     = 0:  successful exit
  *     -     < 0:  if INFO = -i, the i-th argument had an illegal value
@@ -14395,49 +14404,80 @@ mluOpDestroyFFTPlan(mluOpFFTPlan_t fft_plan);
  *                 has been completed, but the factor U is exactly
  *                 singular, and division by zero will occur if it is used
  *                 to solve a system of equations.
+ *
  * @param[in] mode
- *   option to perform operation with pivoting/no pivoting versions 
+ *   option to perform operation with pivoting/no pivoting versions
+ *
+ * @par Return
+ * - ::MLUOP_STATUS_SUCCESS if the workspace size is successfully calculated.
+ * - ::MLUOP_STATUS_BAD_PARAM if there are issues during the calculation allocation.
+ *
+ * @par Data Type
+ * - The supported combinations of data types are shown below:
+ *   - float(x) - float(y)
+ *
+ * @par Data Layout
+ * - The data layout of x should be MLUOP_LAYOUT_ARRAY.
+ * - The data layout of y should be MLUOP_LAYOUT_ARRAY.
+ *
+ * @par Scale Limitation
+ * - The dimension of input tensor must be either 2, 3 or 4.
+ * Considering the size of the GDRAM, the space occupied by the input matrix should not exceed 7GB.
+ *
+ * @par API Dependency
+ * - The allocated extra workspace should be passed to ::mluOpSgetrf2 to perform the LU operation.
+ *
+ * @par Note
+ * - None.
+ *
+ * @par Example
+ * - None.
+ *
+ * @par Reference
+ * - None.
  */
-  mluOpStatus_t MLUOP_WIN_API
-  mluOpSgetrf2(mluOpHandle_t handle,
-              const mluOpTensorDescriptor_t x_desc,
-              void *x,
-              const mluOpTensorDescriptor_t y_desc,
-              void *y,
-              void* workspace,
-              int *ipiv,
-              int *info,
-              int mode);
+mluOpStatus_t MLUOP_WIN_API
+mluOpSgetrf2(mluOpHandle_t handle,
+             const mluOpTensorDescriptor_t x_desc,
+             void *x,
+             const mluOpTensorDescriptor_t y_desc,
+             void *y,
+             void *workspace,
+             int *ipiv,
+             int *info,
+             int mode);
 /*!
- * @brief Calculates the size of the workspace required for the LU decomposition and initializes a workspace pointer. 
- * This function must be called before performing LU decomposition using mluOpCholesky.
+ * @brief Calculates the size of the workspace required for the LU decomposition and initializes a workspace pointer.
+ * This function must be called before performing LU decomposition using mluOpSgetrf2.
  *
  * @param[in] handle
  * Handle to a Cambricon MLUOP context that is used to manage MLU devices and
  * queues in the deformable convolution backward data operation. For detailed information,
  * see ::mluOpHandle_t.
+ *
  * @param[in] input_desc
  * The descriptor for the input tensor for which the LU decomposition will be performed.
+ *
  * @param[out] workspace_size
  * Pointer to a variable where the size of the required workspace will be stored.
- * @param[out] workspace
- * Double pointer to a float, used to allocate memory for the workspace. This pointer will be set to point to the allocated workspace.
  *
  * @par Return
- * - ::MLUOP_STATUS_SUCCESS if the workspace size is successfully calculated and the workspace is successfully allocated,
+ * - ::MLUOP_STATUS_SUCCESS if the workspace size is successfully calculated and the workspace is successfully
+ * allocated,
  * - ::MLUOP_STATUS_EXECUTION_FAILED if there are issues during the calculation or memory allocation.
  *
  * @par Data Type
  * - None.
  *
  * @par Data Layout
- * - None.
+ * - The supported combinations of data types are shown below:
+ * - size_t(size)
  *
  * @par Scale Limitation
- * - None.
+ * - The dimension of input tensor must be either 2, 3 or 4.
  *
  * @par API Dependency
- * - None.
+ * - The allocated extra workspace should be passed to ::mluOpSgetrf2 to perform the LU operation.
  *
  * @par Note
  * - None.
@@ -14448,44 +14488,8 @@ mluOpDestroyFFTPlan(mluOpFFTPlan_t fft_plan);
  * @par Reference
  * - None.
  */
-  mluOpStatus_t MLUOP_WIN_API mluOpGetLUWorkspace(mluOpHandle_t handle,
-                                      const mluOpTensorDescriptor_t input_desc,
-                                      int *workspace_size,
-                                      void **workspace);
-/*!
- * @brief Frees the memory allocated for the LU decomposition workspace.
- * This function should be called to release the workspace memory used by the Cholesky operations after they are no longer needed.
- *
- * @param[in,out] workspace
- * Double pointer to the workspace memory that was allocated by mluOpGetLUWorkspace or another allocation function. 
- * After calling this function, the pointer will be set to NULL to prevent accidental reuse.
- *
- * @par Return
- * - ::MLUOP_STATUS_SUCCESS if the workspace is successfully freed,
- * - ::MLUOP_STATUS_EXECUTION_FAILED if there is an error during the free operation, such as if the pointer is NULL.
- *
- * @par Data Type
- * - None.
- *
- * @par Data Layout
- * - None.
- *
- * @par Scale Limitation
- * - None.
- *
- * @par API Dependency
- * - None.
- *
- * @par Note
- * - None
- *
- * @par Example
- * - None.
- *
- * @par Reference
- * - None.
- */
-  mluOpStatus_t MLUOP_WIN_API mluOpFreeLUWorkspace(void **workspace);
+mluOpStatus_t MLUOP_WIN_API
+mluOpGetLUWorkspaceSize(mluOpHandle_t handle, const mluOpTensorDescriptor_t input_desc, size_t *workspace_size);
 
 #if defined(__cplusplus)
 }
