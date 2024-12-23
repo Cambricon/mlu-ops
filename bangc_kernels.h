9154a65dbee41ae70802f6c218f4166a74928850
wangyuan <wangyuan@cambricon.com>
2024-12-23 17:37:33 +0800

    [Feature](mlu-ops): add mluAdamW.

--- bangc_kernels.h
+++ bangc_kernels.h
@@ -67,6 +67,78 @@ typedef enum {
      due to large scale or inappropriate range of value of input tensor. */
 } bangcKernelsStatus_t;
 
+
+// Group: AdamW
+/*!
+ * @brief Updates each attribute by using AdamW.
+ *
+ * @param[in] queue
+ * A pointer to the cnrtQueue struct holding the information about a queue.
+ * @param[in] lr
+ * A hyperparameter representing the learning rate.
+ * @param[in] beta1
+ * A hyperparameter for updating momentum.
+ * @param[in] beta2
+ * A hyperparameter for updating velocity.
+ * @param[in] bias1
+ * A hyperparameter for updating param.
+ * @param[in] bias2
+ * A hyperparameter for updating param.
+ * @param[in] epsilon
+ * A fraction that prevents the denominator from being zero.
+ * @param[in] weight_decay
+ * A hyperparameter representing weight decay.
+ * @param[in] scale
+ * A hyperparameter of a shrinking gradient.
+ * @param[in] use_nesterov
+ * A parameter that determines whether to use the NAG algorithm.
+ * @param[in] size
+ * A parameter that represents the amount of data for the parameter.
+ * @param[in] param_h
+ * Pointer to the MLU memory that stores the param_h tensor.
+ * @param[in] grad
+ * Pointer to the MLU memory that stores the grad tensor.
+ * @param[in] param
+ * Pointer to the MLU memory that stores the grad tensor.
+ * @param[in] momentum
+ * Pointer to the MLU memory that stores the grad tensor.
+ * @param[in] velocity
+ * Pointer to the MLU memory that stores the grad tensor.
+ * @par Return
+ * - ::BANGC_KERNELS_STATUS_SUCCESS
+ *
+ * @par Data Type
+ * - The supported data types of input and output tensors are as follows:
+ *   - param_h tensor: bfloat16
+ *   - grad tensor: bfloat16
+ *   - param tensor: float
+ *   - momentum tensor: float
+ *   - velocity tensor: float
+ * 
+ * @par Data Layout
+ * - The supported data layouts of \b param tensor, \b param_h tensor, \b momentum tensor, \b velocity tensor, and \b
+ *   grad tensor are as follows:
+ *   - param tensor: ARRAY
+ *   - param_h tensor: ARRAY
+ *   - momentum tensor: ARRAY
+ *   - velocity tensor: ARRAY
+ *   - grad tensor: ARRAY
+ * 
+ * @par Scale Limitation
+ * - None.
+ *
+ * @par API Dependency
+ * - None.
+ *
+ * @par Note
+ * - None.
+ *
+ * @par Example
+ * - None.
+ *
+ * @par Reference
+ * - https://github.com/OpenBMB/BMTrain/blob/6abcf772aa1e120192f7656e55c4adbcde53c886/csrc/cuda/adam_cuda.cu
+ */
 template <typename T>
 bangcKernelsStatus_t BANGC_KERNELS_WIN_API mluAdamW(const cnrtQueue_t queue,
                                                     const float lr,
