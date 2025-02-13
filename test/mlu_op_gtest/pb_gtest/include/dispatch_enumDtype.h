e3e39392d3c4d3e6e7cd98e34b01b92b0a733a47
chqy99 <1216494776@qq.com>
2025-02-13 13:11:44 +0800

    [Feature](mlu-ops): enumType->datatype

---
+++ test/mlu_op_gtest/pb_gtest/include/dispatch_enumDtype.h
@@ -0,0 +1,128 @@
+/*************************************************************************
+ * Copyright (C) [2025] by Cambricon, Inc.
+ *
+ * Permission is hereby granted, free of chvar_arge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *************************************************************************/
+#pragma once
+#include <tuple>
+#include <variant>
+
+#include "mlu_op.h"
+
+namespace Eigen {
+struct half;
+struct bfloat16;
+}  // namespace Eigen
+
+using Eigen::bfloat16;
+using Eigen::half;
+
+template <typename T>
+struct TypeToEnum {
+  static constexpr mluOpDataType_t value = MLUOP_DTYPE_INVALID;
+};
+
+#define typeMapEnum(type, enum)                    \
+  template <>                                      \
+  struct TypeToEnum<type> {                        \
+    static constexpr mluOpDataType_t value = enum; \
+  }
+
+typeMapEnum(float, MLUOP_DTYPE_FLOAT);
+typeMapEnum(half, MLUOP_DTYPE_HALF);
+typeMapEnum(bfloat16, MLUOP_DTYPE_BFLOAT16);
+typeMapEnum(double, MLUOP_DTYPE_DOUBLE);
+typeMapEnum(int8_t, MLUOP_DTYPE_INT8);
+typeMapEnum(int16_t, MLUOP_DTYPE_INT16);
+typeMapEnum(int32_t, MLUOP_DTYPE_INT32);
+typeMapEnum(int64_t, MLUOP_DTYPE_INT64);
+typeMapEnum(uint8_t, MLUOP_DTYPE_UINT8);
+typeMapEnum(uint16_t, MLUOP_DTYPE_UINT16);
+typeMapEnum(uint32_t, MLUOP_DTYPE_UINT32);
+typeMapEnum(uint64_t, MLUOP_DTYPE_UINT64);
+typeMapEnum(bool, MLUOP_DTYPE_BOOL);
+typeMapEnum(std::complex<half>, MLUOP_DTYPE_COMPLEX_HALF);
+typeMapEnum(std::complex<float>, MLUOP_DTYPE_COMPLEX_FLOAT);
+
+/*
+ * Parameter Explanation: T1 must be provided as the default type,
+ *  and ...Types represents the other supported types.
+ *
+ * Key Technology: Variadic templates, using fold expressions.
+ *
+ * Notes: 
+ *  A default type must be provided as the initial variable;
+ *  otherwise, if multiple types do not have a common initial value to assign,
+ *  an error will occur. If an empty type is used as a placeholder, there will
+ *  also be an empty type in the function dispatch, which could result in an
+ *  error due to the missing corresponding instantiation.
+ */
+template <typename T1, typename... Types>
+struct VariantHelper {
+  using Type = std::variant<T1, Types...>;
+
+  static Type create(mluOpDataType_t runEnumDataType) {
+    Type var = T1{};
+    ((runEnumDataType == TypeToEnum<Types>::value ? var = Types{} : T1{}), ...);
+    return var;
+  }
+};
+
+/*
+ * Macro Explanation: Constructs a variant type variable that supports all the
+ *  given types and assigns an initial value based on the enum type obtained
+ *  during runtime.
+ *
+ * Parameter Explanation: var is the variable name of the variant type,
+ *  runEnumDataType is the enum type obtained during runtime,
+ *  T1 is the supported default type,
+ *  and ... represents the other supported types.
+ *
+ * Example: VARIANT_INIT(var, run_dtype, float, half, bfloat16).
+ */
+#define VARIANT_INIT(var, runEnumDataType, dtype, ...) \
+  auto var = VariantHelper<dtype, __VA_ARGS__>::create(runEnumDataType);
+
+/*
+ * Macro Explanation:
+ *  Traverses the variant type and dispatches a template
+ *  function with a variants... during the traversal. It
+ *  should be used in conjunction with the VARIANT_INIT macro.
+ *
+ * Parameter Explanation:
+ *  The T_VAR macro will be used in func_call to specify the types,
+ *  ... are the variables constructed by the VARIANT_INIT macro.
+ *
+ * Key Technology: std::visit.
+ *
+ * Notes: 
+ *   1.Do not declare variables named Types.
+ *   2.When using a function with <>, the entire function call needs to be wrapped in ().
+ */
+#define DISPATCH_VARIANTS(func_call, ...)                          \
+  std::visit(                                                      \
+      [&](auto&&... args) {                                        \
+        using Types = std::tuple<std::decay_t<decltype(args)>...>; \
+        func_call;                                                 \
+      },                                                           \
+      __VA_ARGS__);
+
+// The first variant type is varT(0), and so on.
+#define T_VAR(id) std::tuple_element_t<id, Types>
