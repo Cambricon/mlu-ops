98821623cffa5987bab5e0b260f26f21b7fdbbd3
wangrt1 <wangrt11@163.com>
2025-04-02 21:11:07 +0800

    [Fix](mlu-ops): fix perf analyse gen proto.

--- tools/perf_analyse/analysis_suite/utils/ptpb_helper/proto_helper.py
+++ tools/perf_analyse/analysis_suite/utils/ptpb_helper/proto_helper.py
@@ -18,8 +18,6 @@ def check_mluops_proto():
     proto_dir = os.path.abspath(os.path.realpath(__file__) + "/../../../../../../test/mlu_op_gtest/pb_gtest/mlu_op_test_proto/")
     cwd = os.path.abspath(os.path.realpath(__file__) + "/../../../cfg/")
 
-    cmd1_args = ["pushd", proto_dir, "bash", "build.sh", "popd"]
-    cmd2_args = ["cp", proto_dir + "/mlu_op_test.proto", cwd]
     try:
         # change workspace to proto_dir
         original_dir = os.getcwd()
@@ -31,7 +29,7 @@ def check_mluops_proto():
 
         # cp mlu_op_test.proto to cfg
         os.chdir(original_dir)
-        cmd2_args = ["cp", os.path.join(proto_dir, "mlu_op_test.proto"), cwd]
+        cmd2_args = ["cp", os.path.join(proto_dir, "mlu_op_test_pb2.py"), cwd]
         cmd2_ret = subprocess.run(cmd2_args, check=True)
     except Exception:
         logging.error("run {} and {} failed, please check!".format(" ".join(cmd1_args)," ".join(cmd2_args)))
