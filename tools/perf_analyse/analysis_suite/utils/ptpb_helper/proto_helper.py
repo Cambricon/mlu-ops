131153398c170a71a16cdf2d0b07baa6f0f8f256
wangrt1 <wangrt11@163.com>
2025-04-02 18:19:42 +0800

    [Fix](mlu-ops): fix perf analyse gen proto.

--- tools/perf_analyse/analysis_suite/utils/ptpb_helper/proto_helper.py
+++ tools/perf_analyse/analysis_suite/utils/ptpb_helper/proto_helper.py
@@ -15,13 +15,25 @@ import logging
 def check_mluops_proto():
     # generate mluops_test_pb2.py
     # 生成在 analysis_suite.details/cfg/ 目录下
-    proto_dir = os.path.abspath(os.path.realpath(__file__) + "/../../../../../../test/mlu_op_gtest/pb_gtest/mlu_op_test_proto")
+    proto_dir = os.path.abspath(os.path.realpath(__file__) + "/../../../../../../test/mlu_op_gtest/pb_gtest/mlu_op_test_proto/")
     cwd = os.path.abspath(os.path.realpath(__file__) + "/../../../cfg/")
 
-    cmd_args = ["protoc", "--python_out", cwd, "--proto_path", proto_dir, proto_dir + "/mlu_op_test.proto"]
+    cmd1_args = ["pushd", proto_dir, "bash", "build.sh", "popd"]
+    cmd2_args = ["cp", proto_dir + "/mlu_op_test.proto", cwd]
     try:
-        # check returncode
-        cmd_ret = subprocess.run(cmd_args, check=True)
+        # change workspace to proto_dir
+        original_dir = os.getcwd()
+        os.chdir(proto_dir)
+
+        # gen mlu_op_test.proto
+        cmd1_args = ["bash", "build.sh"]
+        cmd1_ret = subprocess.run(cmd1_args, check=True)
+
+        # cp mlu_op_test.proto to cfg
+        os.chdir(original_dir)
+        cmd2_args = ["cp", os.path.join(proto_dir, "mlu_op_test.proto"), cwd]
+        cmd2_ret = subprocess.run(cmd2_args, check=True)
     except Exception:
-        logging.error("run {} failed, please check!".format(" ".join(cmd_args)))
+        logging.error("run {} and {} failed, please check!".format(" ".join(cmd1_args)," ".join(cmd2_args)))
         raise
+
